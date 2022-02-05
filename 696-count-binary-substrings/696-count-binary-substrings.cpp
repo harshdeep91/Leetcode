181aa4bbe53ec6af722379a18a2f1bd7ce6bb49f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-02-05 07:20:44 +0530

    Time: 27 ms (56.33%), Space: 10.4 MB (81.32%) - LeetHub

---
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int count0=0,count1=0,count=0,i=0;
+        bool zero=s[0]=='0'?1:0;
+        while(i<s.size())
+        {
+            
+            if(s[i]=='0')
+            { 
+                //check for middle one 0010
+                
+                if(count0&&!zero)
+                    count0=0;
+                    zero=1;
+                count0++;
+                if(count1)
+                {
+                    count1--;
+                    count++;
+                }
+            }
+            else
+            {
+                if(count1&&zero)//checking for middle zero 11011
+                    count1=0;
+                     zero=0;
+                 
+                count1++;
+                if(count0)
+                {
+                    count0--;
+                    count++;
+                }
+            }
+            // cout<<s[i]<<"--"<<count0<<" "<<count1<<" "<<count<<" "<<zero<<endl;
+            i++;
+        }
+        return count;
+    }
+};
