25b3f42ae54a337860d1b5b79a7b96955dc04800
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-02-05 07:25:06 +0530

    Time: 45 ms (11.84%), Space: 10.4 MB (96.70%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int count0=0,count1=0,count=0,i=0;
-        bool zero=s[0]=='0'?1:0;
-        while(i<s.size())
-        {
-            
-            if(s[i]=='0')
-            { 
-                //check for middle one 0010
-                
-                if(count0&&!zero)
-                    count0=0;
-                    zero=1;
-                count0++;
-                if(count1)
-                {
-                    count1--;
-                    count++;
-                }
-            }
-            else
-            {
-                if(count1&&zero)//checking for middle zero 11011
-                    count1=0;
-                     zero=0;
-                 
-                count1++;
-                if(count0)
-                {
-                    count0--;
-                    count++;
-                }
-            }
-            i++;
-        }
-        return count;
+        int curConsecutive = 1, prevConsecutive = 0, ans = 0;
+  for(int i = 1; i < size(s); i++) {
+  	if(s[i] != s[i - 1]) 
+  		ans += min(prevConsecutive, curConsecutive),
+  		prevConsecutive = curConsecutive,       
+  		curConsecutive = 1;                         
+  	else 
+  		curConsecutive++; 
+  }
+  ans += min(prevConsecutive, curConsecutive); 
+  return ans;
     }
 };
