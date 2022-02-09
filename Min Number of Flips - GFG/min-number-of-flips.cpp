206764b5458a2cf4fc2d1034deb228d52667ab9f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-02-09 06:43:42 +0530

    Added solution - LeetHub

---
+++ Min Number of Flips - GFG/min-number-of-flips.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int minFlips (string s);
+int32_t main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+
+        cout << minFlips (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int minFlips (string S)
+{
+    bool flag=0;
+    int ans0=0,ans1=1;
+    for(int i=0;i<S.size();i++)
+    {
+        //ans0 odd place =1
+        //ans1 odd place=0
+        if(i&1&&S[i]!='1')//odd means 0
+        ans0++;
+        else if(!(i&1)&&S[i]!='0')
+        ans0++;
+        // {
+        //     S[i]=='0'?ans1:ans0++;
+        // }
+        // else
+        // {
+        //     S[i]=='1'?ans1:ans0++;
+        // }
+    }
+    ans1=S.size()-ans0;
+    // cout<<ans1<<" "<<ans0<<endl;
+    return min(ans0,ans1);
+    
+}
