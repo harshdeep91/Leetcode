9e321472b6e80ea85cbeeb778aef0d588fbdee59
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-03-01 06:57:17 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -17,12 +17,24 @@ int lowerbound(int arr[],int n,int x)
     }
     return l;
 }
-
+int upperbound(int arr[],int n,int x)
+{
+    int l=0,h=n-1,m;
+    while(l<=h)
+    {
+        m=(l+h)/2;
+        if(arr[m]<=x)
+        l=m+1;
+        else
+        h=m-1;
+    }
+    return l;
+}
 vector<int> find(int arr[], int n , int x )
 {
     int l=lowerbound(arr,n,x);
     // cout<<l<<endl;
-    int h=upper_bound(arr,arr+n,x)-arr;
+    int h=upperbound(arr,n,x);
     // cout<<h<<endl;
     h--;
     if(l>h)
