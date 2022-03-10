d85f202e1507704056cf700cc0404cc4d67b31b7
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-03-10 06:43:04 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -14,9 +14,10 @@ class Solution {
 		        {
 		            if(i==0||j==0)
 		            dp[i][j]=0;
-		            else 
-		             dp[i][j]=max(dp[i-1][j],max(dp[i][j-1],(str[i-1]==str[j-1]&&i!=j)?dp[i-1][j-1]+1:dp[i-1][j-1]));
-		          //   cout<<dp[i][j]<<" ";
+		            else if(str[i-1]==str[j-1]&&i!=j)
+		            dp[i][j]=dp[i-1][j-1]+1;
+		            else
+		             dp[i][j]=max(dp[i-1][j],dp[i][j-1])	;	          //   cout<<dp[i][j]<<" ";
 		        }
 		      //  cout<<endl;
 		    }
