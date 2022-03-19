cbabc64dde3154be6f00d4e45bb22ccf1f6a1b3e
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-03-19 07:52:29 +0530

    Added solution - LeetHub

---
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -0,0 +1,84 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    vector<vector<int>> mat;
+    vector<string> ans;
+    int N;
+    static bool check(int &i,int &j)
+    {
+        
+        return true;
+        return false;
+    }
+    void solve(int i,int j,string s)
+    {
+        if(i==N-1&&j==N-1&&mat[i][j])
+        {
+            ans.push_back(s);
+            return ;
+        }
+        if(i>=0&&i<N&&j>=0&&j<N&&mat[i][j]==1)
+        {
+            mat[i][j]=0;
+           
+           solve(i-1,j,s+"U");
+           solve(i+1,j,s+"D");
+           solve(i,j-1,s+"L");
+           solve(i,j+1,s+"R");
+           mat[i][j]=1;
+        }
+        
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        mat.clear();
+        mat=m;
+        ans.clear();
+        N=n;
+        // for(int i=0;i<mat.size();i++)
+        // {
+        //     for(int j=0;j<mat[0].size();j++)
+        //     cout<<mat[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        solve(0,0,"");
+        return ans;
+    }
+};
+
+    
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
