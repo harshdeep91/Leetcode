475f44efead182a4208a3b6b06550398ce14eb98
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-03-30 07:00:54 +0530

    Added solution - LeetHub

---
+++ Travelling Salesman Problem - GFG/travelling-salesman-problem.cpp
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+public:
+int solve(int j,int count,unordered_map<string,int> &dp,vector<vector<int>>&cost,vector<int>&vis)
+{
+    
+    
+    if(count==cost.size())//we selected everything
+    {
+        return cost[j][0];
+    }
+    vis[j]=1;
+    int low=INT_MAX;
+    for(int i=0;i<cost.size();i++)
+    {
+        
+        if(!vis[i]&&cost[j][i])
+        {
+            // string s;
+            // for(int m=0;m<cost.size();m++)
+            // {
+            //     if(!vis[m])
+            //     s.push_back(m+'0');
+            // }
+            
+            // if(!dp[s])
+            // {
+                // cout<<s<<endl;
+            //   dp[s]=solve(i,count+1,dp,cost,vis)+cost[j][i]; 
+            // }
+            
+            // cout<<j<<" "<<i<<" "<<cost[j][i]<<" "<<low<<endl;
+            // low=min(low,dp[s]);
+            low=min(low,solve(i,count+1,dp,cost,vis)+cost[j][i]);
+            
+        }
+    }
+    vis[j]=0;
+    // cout<<low<<endl;
+    return low;
+}
+int total_cost(vector<vector<int>>cost){
+    int n=cost.size();
+   unordered_map<string,int> dp;
+    vector<int>vis(n,0);
+    vis[0]=1;
+    return solve(0,1,dp,cost,vis);
+}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>cost(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)cin >> cost[i][j];
+		Solution obj;
+		int ans = obj.total_cost(cost);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
