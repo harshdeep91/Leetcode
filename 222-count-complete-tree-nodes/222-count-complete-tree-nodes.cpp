f0157066445aac0e046f82ad2c2ca4e3901afd11
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-04-07 07:05:46 +0530

    Time: 28 ms (93.77%), Space: 30.9 MB (64.60%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -11,39 +11,16 @@
  */
 class Solution {
 public:
-    int mx,mxv;
-    int countNode(TreeNode* root,int count,int h)
-    {
-        
-        if(root)
-        {
-            // cout<<h<<" "<<count<<" "<<root->val<<endl;
-            if(countNode(root->right,count*2+1,h+1))
-                return 1;
-            if(countNode(root->left,count*2,h+1))
-                return 1;
-            if(h==mx)
-            {
-                mxv=count;
-                return 1;
-            }
-        }
-        return 0;
-    }
+   
     int countNodes(TreeNode* root) {
-        if(!root)
-            return 0;
+       if (!root) return 0;
         
-        mx=1;
-        mxv=1;
-        TreeNode *p=root;
-        while(p)
-        {
-            mx++;
-            p=p->right;
-        }
-            mxv=pow(2,mx-1)-1;
-       countNode(root,1,1);
-        return mxv;
+        int l = 1, r = 1;
+        TreeNode *ptr_r = root, *ptr_l = root;
+        while (ptr_l = ptr_l->left) l++;
+        while (ptr_r = ptr_r->right) r++;
+        
+        if (l == r) return pow(2, l) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
