cf160f6c5a4e790a7d764258c150ffa6d675b1d9
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-04-10 07:19:53 +0530

    Time: 42 ms (5.76%), Space: 8.6 MB (40.63%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        
-        sort(citations.begin(),citations.end());
         int n=citations.size(),j,notgreater,h=n;
-        // for(int i=n-1;i>=0;i--)
-        // {
-        //     if(citations[i]<=n)
-        //     {
-        //         h=citations[i];
-        //         break;
-        //     }
-        // }
-        
         for(int i=h;i>=0;i--)
         {
            
@@ -22,14 +11,13 @@ public:
             {
                 // if(i==3)
                 //     cout<<citations[j]<<" "<<notgreater<<endl;
-                if(notgreater)
-                {
-                    if(citations[j]>i)
-                        break;
-                    notgreater--;
-                }    
-                else if(citations[j]<i)
-                    break;
+                    if(citations[j]<i)
+                    {
+                        if(notgreater)
+                            notgreater--;
+                        else
+                            break;
+                    }
             }
             if(j==n)
                 return i;
