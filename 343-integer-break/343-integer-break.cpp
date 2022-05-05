e20593693882ee94df6fe8a0824ac0c782fdfb87
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-05 10:36:09 +0530

    Time: 5 ms (16.89%), Space: 5.8 MB (91.41%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
-    void solve(int j,int product,int n,int &mx,int k)
-    {
-        if(n==0&&k>1)
-        {
-            mx=max(product,mx);
-            return ;
-        }
-        if(n<0)
-            return;
-        while(j<=n)
-        {
-            solve(j,product*j,n-j,mx,k+1);
-            j++;
-        }
-    }
     int integerBreak(int n){
-        int mx=0,val=n;
-        solve(1,1,n,mx,0);
-        return mx;
+          if (n <= 3) return 1 * (n - 1);
+        int res = 1;
+        while (n >= 5) n -= 3, res *= 3;
+        return res * n;
     }
 };
