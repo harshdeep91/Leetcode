34379bd239135b444fc60b24c63bf0c09235c626
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-08 09:37:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.67%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-   int getSum(int a, int b) {
-        while(b) {
-            unsigned c = a&b;
-            a ^= b;
-            b = c << 1;
+    int getSum(int a, int b) {
+            int carry,ans,i=2;
+        while(b)
+        {
+            
+            int ans = a ^ b;   //01
+            unsigned carry = a & b;//10
+            carry <<= 1;      //100
+            a = ans|carry;    //101
+            b = carry&ans;
+            // cout<<a<<" "<<b<<endl;
         }
         return a;
-}
+    }
 };
