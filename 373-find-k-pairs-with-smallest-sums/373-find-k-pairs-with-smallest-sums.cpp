fbd5565cb567fe869d70269925ec9db0350f1121
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-09 07:58:15 +0530

    Time: 216 ms (96.59%), Space: 106.3 MB (96.83%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,46 +1,23 @@
-struct node{
-    int sum,first, second;
-};
-class cmp{
-  public:
-    bool operator()(node A,node B){
-        return A.sum <= B.sum;
-    }
-};
 class Solution {
 public:
-    
+    #define pii pair<int,int>
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<node,vector<node>,cmp> p;
-        int i = 0, j = 0;
-        while(i<nums1.size())
+        priority_queue<pair<int,pii>,vector<pair<int,pii>>,greater<pair<int,pii>>>pq;
+        pq.push({nums1[0]+nums2[0],{0,0}});
+        vector<vector<int>>ans;
+        while(!pq.empty() and k--)
         {
-            j = 0;
-            while(j<nums2.size())
-            {
-                // cout << nums1[i] << " " << nums2[j] << endl;
-                if(p.size()<k)
-                    p.push({nums1[i] + nums2[j], nums1[i], nums2[j]});
-                else
-                {
-                    if(p.top().sum>nums1[i]+nums2[j])
-                    {
-                        p.pop();
-                        p.push({nums1[i] + nums2[j], nums1[i], nums2[j]});
-                    }
-                    else
-                        break;
-                }
-                    j++;
-            }
-            i++;
+            int x=pq.top().first;
+            int left=pq.top().second.first;
+            int right=pq.top().second.second;
+            ans.push_back({nums1[left],nums2[right]});
+            pq.pop();
+            if(right<nums2.size()-1 and left==0)
+                pq.push({nums1[left]+nums2[right+1],{left,right+1}});
+            if(left<nums1.size()-1)
+                pq.push({nums1[left+1]+nums2[right],{left+1,right}});
+            
         }
-        vector<vector<int>> v;
-        while(!p.empty())
-            {
-                v.push_back({p.top().first, p.top().second});
-                p.pop();
-            }
-                return v;
+        return ans;
     }
 };
