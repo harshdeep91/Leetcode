68deff89636b6b205380380189df2b43b37bc69f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-09 09:43:06 +0530

    Time: 69 ms (85.88%), Space: 6.1 MB (81.46%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,24 +1,31 @@
 
 class Solution {
-    vector<vector<int>> dp;
 public:
-    int solve(int s,int e)
-    {
-        if(s>=e)
-            return 0;
-        if(dp[s][e])
-            return dp[s][e];
-        int ans=INT_MAX,val;
-        for(int i=s;i<=e;i++)
-        {
-            val=max(solve(s,i-1),solve(i+1,e))+i;
-            ans=min(ans,val);
-        }
-        return dp[s][e]=ans;
-    }
+   
     int getMoneyAmount(int n) {
-        dp.clear();
-        dp.resize(n+1,vector<int>(n+1,0));
-        return solve(1,n);
+        int dp[n+1][n+1];
+        if(n==1)
+            return 0;
+        for(int g=1;g<=n;g++)
+        {
+            // cout<<g<<endl;
+            for(int i=1,j=g;j<=n;j++,i++)
+            {
+                // cout<<i<<" "<<j<<endl;
+                if(g==1||g==2)
+                    dp[i][j]=i;
+                else if(g==3)
+                    dp[i][j]=i+1;
+                else
+                {
+                    dp[i][j]=INT_MAX;
+                    for(int k=i+1;k<j-1;k++)
+                        dp[i][j]=min(dp[i][j],max(dp[i][k-1],dp[k+1][j])+k);
+                }
+                // cout<<dp[i][j]<<" ";
+            }
+            // cout<<endl;
+        }
+        return dp[1][n];
     }
 };
