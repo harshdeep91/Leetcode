ff07116c663d89cccbbba0a59b7cc95c30f0cbd0
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-13 07:58:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.83%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -1,19 +1,37 @@
 class Solution {
-    map<unsigned,int> dp;
 public:
-    int solve(unsigned n)
-    {
-        // cout << n<<" ";
-        if(n==1)
-            return 0;
-            if(dp[n])
-                return dp[n];
-        if(n&1)
-            return dp[n]=min(solve(n + 1),solve (n - 1))+1;
-        else
-        return dp[n]= solve(n / 2) + 1;
-    }
     int integerReplacement(int n) {
-        return solve(n);
+         if(n==INT_MAX) return 32;
+        int count  = 0;
+        while(n>3){
+            if(n%2==0){
+                // if n is even
+                n=n>>1;
+            }else{
+                // if n is odd
+                int increment = n+1;
+                int decrement = n-1;
+                int unsetBits = 0;
+                
+                while(!(increment&1)){
+                    unsetBits++;
+                    increment = increment>>1;
+                }
+                while(!(decrement&1)){
+                    unsetBits--;
+                    decrement = decrement>>1;
+                }
+                if(unsetBits>0){
+                    n = n+1;
+                }else{
+                    n = n-1;
+                }
+            }
+            count++;
+        }
+        
+        if(n<=3) count+=(n-1);
+        
+        return count;
     }
 };
