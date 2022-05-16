dbc0bec8b7305d571379cb93f7b41cebdca03e6b
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-16 09:55:12 +0530

    Time: 873 ms (25.00%), Space: 74.8 MB (50.00%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -1,44 +1,40 @@
 
 class Solution {
 public:
-    int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
-        
-        int n = tiles.size();
-        vector<int> start, end;
-        for(vector<int> vec : tiles) {
-            start.push_back(vec[0]);
-            end.push_back(vec[1]);
-        }
-        
-        sort(start.begin(), start.end());
-        sort(end.begin(), end.end());
-        
-        int total = 0;
-        vector<int> pref(n);
-        for(int i=0; i<n; i++) {
-            total += end[i]-start[i]+1;
-            pref[i] = total;
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++) {
-            int last = start[i]+carpetLen-1;
-            
-            if(end[n-1] <= last) {
-                int curr = pref[n-1];
-                if(i > 0)   curr -= pref[i-1];
-                
-                ans = max(ans, curr);
-            }
-            else {
-                int pos = upper_bound(end.begin(), end.end(), last) - end.begin();
-                int curr = pref[pos-1];
-                if(i > 0)   curr -= pref[i-1];
-                
-                if(last >= start[pos])  curr += last-start[pos]+1;
-                ans = max(ans, curr);
-            }
-        }
-        
-        return ans;
+    int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetlen) {
+         sort(tiles.begin(),tiles.end());
+        // for(auto x:tiles)
+        //     cout<<x[0]<<" "<<x[1]<<"\n";
+    int first=tiles[0][0],last=first+carpetlen-1,n=tiles.size(),prefixsum[n],counter,useless=0,ans=0,i=0; prefixsum[0]=0;
+    vector<pair<int, int>> v;
+    v.push_back({tiles[0][0], tiles[0][1]});
+    for (int i = 1; i < n; i++)
+    {
+        prefixsum[i] = prefixsum[i - 1] + (tiles[i][0] - tiles[i - 1][1] - 1);
+        v.push_back({tiles[i][0], tiles[i][1]});
+    }
+        // cout<<n;
+    if(carpetlen>tiles[n-1][1]||last>=tiles[n-1][1])
+    return tiles[n-1][1]-tiles[0][0]+1-prefixsum[n-1];
+    if(n==1)
+        return min(carpetlen, tiles[0][1] - tiles[0][0] + 1);
+    while (last <= tiles[n - 1][1])
+    {
+        pair<int, int> k = {last, INT_MIN};
+        auto it = upper_bound(v.begin(), v.end(),k) - v.begin();
+        if(it==n||last!=v[it].first)
+            it--;
+        // cout<<it<<" ";
+            useless = prefixsum[it] - prefixsum[i];
+            last = min(last, v[it].second);
+            counter = last - first + 1 - useless;
+            ans = max(ans, counter);
+        i++;
+        if(i==n)return ans;
+            first=v[i].first;
+            last = first + carpetlen - 1;
+        // cout<<last<<endl;
+    }
+    return ans;
     }
 };
