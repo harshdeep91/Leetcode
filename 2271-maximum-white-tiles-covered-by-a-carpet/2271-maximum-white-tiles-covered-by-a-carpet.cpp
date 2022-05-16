86c9f4387a922b80089c47f50465ebb87d41c3db
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-16 10:01:59 +0530

    Time: 477 ms (50.00%), Space: 69.6 MB (100.00%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -1,40 +1,40 @@
+/*Intuition: it's always better to place a carpet at the beginning of a white tile range. The reason is, if you shift a carpet one tile forward, you might cover another white tile, but you definitely uncover the previous white tile.
 
+So, we sort tile ranges, and check how many white tiles we can cover, starting from the beginning of each range.
+
+To avoid TLE, we "drag" our carpet over ranges using a sliding window. Applying sliding window is a bit tricky here:
+
+We track the current placement of the carpet (tiles[j][0]).
+When we can cover tiles[i], we add those tiles to cover.
+	Otherwise, we partially cover tiles[i]
+	We remove tiles[j] from cover.
+	We move the placement of the carpet (++j)
+*/
 class Solution {
 public:
-    int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetlen) {
-         sort(tiles.begin(),tiles.end());
-        // for(auto x:tiles)
-        //     cout<<x[0]<<" "<<x[1]<<"\n";
-    int first=tiles[0][0],last=first+carpetlen-1,n=tiles.size(),prefixsum[n],counter,useless=0,ans=0,i=0; prefixsum[0]=0;
-    vector<pair<int, int>> v;
-    v.push_back({tiles[0][0], tiles[0][1]});
-    for (int i = 1; i < n; i++)
-    {
-        prefixsum[i] = prefixsum[i - 1] + (tiles[i][0] - tiles[i - 1][1] - 1);
-        v.push_back({tiles[i][0], tiles[i][1]});
-    }
-        // cout<<n;
-    if(carpetlen>tiles[n-1][1]||last>=tiles[n-1][1])
-    return tiles[n-1][1]-tiles[0][0]+1-prefixsum[n-1];
-    if(n==1)
-        return min(carpetlen, tiles[0][1] - tiles[0][0] + 1);
-    while (last <= tiles[n - 1][1])
-    {
-        pair<int, int> k = {last, INT_MIN};
-        auto it = upper_bound(v.begin(), v.end(),k) - v.begin();
-        if(it==n||last!=v[it].first)
-            it--;
-        // cout<<it<<" ";
-            useless = prefixsum[it] - prefixsum[i];
-            last = min(last, v[it].second);
-            counter = last - first + 1 - useless;
-            ans = max(ans, counter);
-        i++;
-        if(i==n)return ans;
-            first=v[i].first;
-            last = first + carpetlen - 1;
-        // cout<<last<<endl;
-    }
-    return ans;
+    int maximumWhiteTiles(vector<vector<int>>& t, int len) 
+	{
+	    int res = 0, j = 0, cover = 0;//cover is length of white tiles covered by carpet in current window
+	    sort(t.begin(), t.end());
+	    for (int i = 0; i < t.size();)//carpet starts from j and end at i
+	    {
+	        if (j == i || t[j][0] + len > t[i][1]) //carpet starting from j is exceeding this window(as this window is ending at i)
+			{
+	            cover += t[i][1] - t[i][0] + 1;//this window is being extended to cover this patch also
+	            res = max(res, cover);
+	            ++i;//let this window try to cover next patch by expanding its right end
+	        }
+	        else//carpet starting from jth patch is not able to cover this whole patch at i 
+			{
+	            res = max(res, cover + max(0, t[j][0] + len - t[i][0]));
+	            					/* Partial length covered by 
+	            					   carpet in this patch
+	            					*/
+	            cover -= (t[j][1] - t[j][0] + 1);//window will slide towards right hence remove the contribution of cover given by left most 
+	            								//patch of current window. 
+	            ++j;//slide the window towards its right
+	        }
+	    }
+	    return min(res,len);
     }
 };
