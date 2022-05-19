093cf5d4aa3bfbbba8d7a2648e7c86d944a697d5
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-19 18:16:16 +0530

    Time: 8 ms (84.30%), Space: 6.9 MB (86.13%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,13 @@
 class Solution {
-    vector<int> a;
 public:
-    int maxyt()
-    {
-        int mx=0;
-        for(int i=0;i<26;i++)
-            mx=max(mx,a[i]);
-        return mx;
-    }
     int characterReplacement(string s, int k) {
-        int i=0,j=0,mx=0,t,count=0,maxyt=0;
-        a.resize(26,0);
+        int i=0,j=0,mx=0,maxyt=0,a[26]={0};
         while(j<s.size())
         {
             a[s[j]-'A']++;
             maxyt=max(maxyt,a[s[j]-'A']);
             if(j-i+1-maxyt>k)
-            {
                 a[s[i++]-'A']--;
-                // cout<<count++<<" ";
-            }
-            // cout<<endl;
             mx=max(mx,j-i+1);
             j++;
         }
