7cdbebf809e016b5520c4d4b6e3d0d23a1afa147
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-20 16:50:30 +0530

    Time: 80 ms (81.24%), Space: 25.5 MB (84.87%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,40 +1,36 @@
 class Solution {
-    vector<vector<int>> v;
 public:
-     static bool cmp(vector<int> &a,vector<int> &b)
-    {
-        if(a[0]<b[0]||(a[0]==b[0]&&a[1]<b[2]))
-            return 1;
-        return 0;
-    }
-    int upperbound(int x)
-    {
-        int l=x,h=v.size()-1;
-        while(l<=h)
-        {
-            int m=(l+h)>>1;
-            if(v[m][0]<v[x][1])//start at m< end at x
-                l=m+1;
-            else
-                h=m-1;
-        }
-        return l;
-    }
+    int bound(vector<pair<int,int>> &v,int val,int n){
+    int l=0;int r=n;int mid;
+        
+    while(l<r){
+        mid=(l+r)/2;
+        
+        if(v[mid].first>=val)r=mid;
+        else l=mid+1;    
+    }   
+ return l<n ? v[l].second : -1;   
+}
+    
+    
+    
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        v.clear();
-        vector<int> ans(intervals.size(),-1);
-        for(int i=0;i<intervals.size();i++)
-            v.push_back({intervals[i][0],intervals[i][1],i});
-        sort(v.begin(),v.end(),cmp);
-        for(int i=0;i<intervals.size();i++)
-        {
-                 int j=upperbound(i);
-                if(j<intervals.size())
-                {
-                    // cout<<v[i][0]<<" "<<v[i][1]<<endl;
-                    ans[v[i][2]]=v[j][2];
-                }
+    int n=intervals.size();
+        vector<pair<int,int>>v;
+        
+        for(int i=0;i<n;i++){
+          v.push_back({intervals[i][0],i}); 
         }
-        return ans;
+        
+        sort(v.begin(),v.end());
+        vector<int>ans;
+        
+        for(int i=0;i<n;i++){
+            int result=bound(v,intervals[i][1],n);
+            ans.push_back(result);
+        }
+        
+        
+     return ans;   
     }
 };
