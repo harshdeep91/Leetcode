6c5ee243835427d0bf24363eeefabaf62329a181
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-21 08:28:06 +0530

    Time: 671 ms (68.16%), Space: 115.8 MB (83.07%) - LeetHub

--- 447-number-of-boomerangs/447-number-of-boomerangs.cpp
+++ 447-number-of-boomerangs/447-number-of-boomerangs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numberOfBoomerangs(vector<vector<int>>& points) {
-        unordered_map<double,int> m;
+        unordered_map<long,int> m;
         int count=0;
         for(int i=0;i<points.size();i++)
         {
@@ -11,16 +11,11 @@ public:
                 if(i==j)continue;
                 int x=abs(points[i][0]-points[j][0]);
                 int y=abs(points[i][1]-points[j][1]);
-                double dis=sqrt(x*x+y*y);
+                long dis=x*x+y*y;
                 m[dis]++;
                 if(m[dis]>1)//it means we got a new one
                     count+=m[dis]*2-2;
-                    
-        // cout<<points[i][0]<<","<<points[i][1]<<") ("<<points[j][0]<<","<<points[j][1]<<") "<<x<<" "<<y<<"---"<<dis<<" "<<count<<endl;
             }
-            // for(auto x:m)
-            //     if(x>1)
-            //         count+=
         }
        return count;
     }
