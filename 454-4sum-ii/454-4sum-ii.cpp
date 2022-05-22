c80b69350b4f912d54288fa2119d6a9516315eee
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-22 18:33:07 +0530

    Time: 506 ms (15.49%), Space: 24.8 MB (25.68%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-      map<int,int> f,s;
-        for(int i=0;i<nums1.size();i++)
-            for(int j=0;j<nums2.size();j++)
-                f[nums1[i]+nums2[j]]++;
-        for(int i=0;i<nums3.size();i++)
-            for(int j=0;j<nums4.size();j++)
-                s[nums3[i]+nums4[j]]++;
-        int count=0;
-        for(auto x:f)
-                count+=s[-x.first]*x.second;
+        // unordered_map<int,int> f,s;
+        // for(int i=0;i<nums1.size();i++)
+        //     for(int j=0;j<nums2.size();j++)
+        //         f[nums1[i]+nums2[j]]++;
+        // for(int i=0;i<nums3.size();i++)
+        //     for(int j=0;j<nums4.size();j++)
+        //         s[nums3[i]+nums4[j]]++;
+        // int count=0;
+        // for(auto x:f)
+        //         count+=s[-x.first]*x.second;
+        // return count;
+           map<int,int> mp;
+        for(int k : nums3)
+            for(int l : nums4)
+                mp[k + l]++;
+        int count = 0;
+        for(int i : nums1)
+            for(int j : nums2)
+                        count += mp[-(i + j)];
         return count;
             
     }
