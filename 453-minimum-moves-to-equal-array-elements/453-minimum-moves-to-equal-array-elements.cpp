4820b140a4ce0edfdd851c878c4eb4f5377e094d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-22 18:10:35 +0530

    Time: 62 ms (36.59%), Space: 28.3 MB (83.90%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        int count=0,mi=INT_MAX;
-        for(int i=0;i<nums.size();i++)
-            mi=min(mi,nums[i]);
-            for(int i=0;i<nums.size();i++)
-             count+= nums[i]-mi;
-        return count;
+         int m=INT_MAX;
+			for(int n:nums) m = min(m,n);
+			int ans=0;
+			for(int n:nums) ans+=n-m;
+			return ans;
     }
 };
-
-//brute force every time choose min and max and count+=max-min and update array with max-min 
