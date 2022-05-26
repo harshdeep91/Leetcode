c9bc197ca27d8eb678390b7e2fbfe2260d54c5f1
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-26 09:29:21 +0530

    Time: 119 ms (25.51%), Space: 25.8 MB (26.03%) - LeetHub

--- 475-heaters/475-heaters.cpp
+++ 475-heaters/475-heaters.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    bool solve(int &m,vector<int>& houses, vector<int>& heaters) {
-        int i=0,j=0;
-        //left to right
-        while(i<houses.size()&&j<heaters.size())
-        {
-            if(heaters[j]-m<=houses[i]&&houses[i]<=heaters[j]+m)
-                i++;
-            else
-                j++;
+
+    int findRadius(vector<int>& A, vector<int>& H) {
+        sort(A.begin(), A.end());
+        sort(H.begin(), H.end());
+        vector<int> res(A.size(), INT_MAX); 
+        
+        // For each house, calculate distance to nearest RHS heater
+        for (int i = 0, h = 0; i < A.size() && h < H.size(); ) {
+            if (A[i] <= H[h]) { res[i] = H[h] - A[i]; i++; }
+            else { h++; }
         }
-        if(i==houses.size())return 1;
-        return 0;
-    };
-    int findRadius(vector<int>& houses, vector<int>& heaters) {
-        sort(houses.begin(),houses.end()),sort(heaters.begin(),heaters.end());
-        int l=0,h=max(abs(heaters.back()-houses.front()),abs(heaters.front()-houses.back())),m;
-        while(l<=h)
-        {
-            // cout<<l<<" "<<h<<endl;
-            m=l+(h-l)/2;
-            if(solve(m,houses,heaters))
-                h=m-1;
-            else
-                l=m+1;
+        
+        // For each house, calculate distance to nearest LHS heater
+        for (int i = A.size()-1, h = H.size()-1; i >= 0 && h >= 0; ) {
+            if (A[i] >= H[h]) { res[i] = min(res[i], A[i] - H[h]); i--; }
+            else { h--; }
         }
-        return l;
+       
+        return *max_element(res.begin(), res.end());
     }
 };
