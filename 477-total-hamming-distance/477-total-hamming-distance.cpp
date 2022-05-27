c64df5b4a67c341fcf7806d93a1605a43acfac64
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-27 08:25:01 +0530

    Time: 61 ms (69.15%), Space: 19 MB (92.16%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -5,7 +5,10 @@ public:
         for (int j=0;j<32;j++) {
             int bitCount = 0;
             for (int i=0;i<n;i++) 
-                bitCount += (nums[i] >> j) & 1;
+            {
+                bitCount +=nums[i]&1;
+                nums[i]>>=1;
+            }
             total += bitCount*(n - bitCount);
         }
         return total;
