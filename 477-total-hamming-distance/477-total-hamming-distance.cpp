b0429feac4aede108ca289ead05793b5c6773966
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-27 08:15:57 +0530

    Time: 54 ms (83.03%), Space: 19 MB (92.16%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int a[32]={0},count=0,x,pos,n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            x=nums[i],pos=31;
-            while(x)
-            {
-                a[pos--]+=x&1;
-                x>>=1;
-            }
+     int total = 0, n = nums.size();
+        for (int j=0;j<32;j++) {
+            int bitCount = 0;
+            for (int i=0;i<n;i++) 
+                bitCount += (nums[i] >> j) & 1;
+            total += bitCount*(n - bitCount);
         }
-        for(int i=0;i<32;i++)
-            count+=(a[i]*(n-a[i]));
-            // cout<<a[i];
-        // cout<<endl;
-        return count;
+        return total;
     }
     
 };
