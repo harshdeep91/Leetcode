6b9315b8774fc5a2b88d65e49acde91a054a2d2d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-05-31 08:24:59 +0530

    Time: 64 ms (92.14%), Space: 19.1 MB (90.25%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        if(pow(2,k)-1>s.size())return 0;
+        // if(pow(2,k)-1>s.size())return 0;
          unsigned p=1,t=k,j=0;
-            // long long sum=pow(2,k)-1;
-            //  sum=(sum*(sum+1))/2;  
-        
             while(--t)//mask making
             {
                 p<<=1;
                 p|=1;
             }
         vector<bool> a(p+1,0);
-        // cout<<sum<<" "<<t<<" "<<p<<endl;
          while(j<s.size())
          {
              
@@ -20,7 +16,6 @@ public:
              if(++j>=k)
              {
                  a[t]=1;  
-                 // cout<<t<<" ";
                  t<<=1;
                  t&=p;
              }else
@@ -28,9 +23,6 @@ public:
          }
         j=-1;
         while(++j<=p&&a[j]==1);
-        // while(++j<=p)
-        //     cout<<a[j]<<" ";
-        // cout<<j<<" "<<p<<endl;
         return j>p;
     }
 };
