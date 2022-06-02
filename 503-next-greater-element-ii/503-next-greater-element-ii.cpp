983a2effbe52220c24990e3b192015e590894cf6
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-02 18:20:45 +0530

    Time: 39 ms (79.68%), Space: 23.9 MB (61.16%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans(n,INT_MIN);
-        for(int i=0;i<n;i++)
-            nums.push_back(nums[i]);
+        // for(int i=0;i<n;i++)
+        //     nums.push_back(nums[i]);
         stack<int> s;
-        for(int i=nums.size()-1;i>=0;i--)
+        for(int i=2*n-1;i>=0;i--)
         {
-            while(!s.empty()&&nums[i]>=s.top())
+            while(!s.empty()&&nums[i%n]>=s.top())
                 s.pop();
             if(!s.empty())
                 ans[i%n]=max(ans[i%n],s.top());
-            s.push(nums[i]);
+            s.push(nums[i%n]);
         }
         for(int i=0;i<n;i++)
             if(ans[i]==INT_MIN)ans[i]=-1;
