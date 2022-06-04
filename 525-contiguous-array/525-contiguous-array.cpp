30dda8e0e43f44bf65ae7974c7c453adc8b34244
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-04 09:39:35 +0530

    Time: 158 ms (71.57%), Space: 83.8 MB (53.94%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -6,7 +6,7 @@ public:
         int mx=0,sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            sum+=(nums[i]?1:-1);
+            sum+=nums[i]?1:-1;
             if(m.find(sum)==m.end())
                 m[sum]=i;
             mx=max(mx,i-m[sum]);
