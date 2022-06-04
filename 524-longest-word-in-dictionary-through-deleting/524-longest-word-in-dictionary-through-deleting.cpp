b6c123f4d791bb1acc46e912dabc27b4b623482d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-04 09:20:55 +0530

    Time: 91 ms (42.84%), Space: 17.6 MB (60.63%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.cpp
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.cpp
@@ -5,15 +5,18 @@ public:
             return a.size()>b.size()||(a.size()==b.size()&&a<b);
     }
     string findLongestWord(string s, vector<string>& dictionary) {
-        sort(dictionary.begin(),dictionary.end(),cmp);
+        // sort(dictionary.begin(),dictionary.end(),cmp);//n*k*log(n)
+        string ans;
         for(auto x:dictionary)
         {
             int j=0,i=0;
             while(i<x.size()&&j<s.size())
                 if(x[i]==s[j])i++,j++;
                 else j++;
-            if(i==x.size())return x;
+            if(i==x.size()&&(x.size()>ans.size()||(x.size()==ans.size()&&x<ans)))
+                ans=x;
+                
         }
-        return "";
+        return ans;
     }
 };
