dbf7bd8f788bf40331eb141bf6ec5fb4579561de
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-05 19:56:46 +0530

    Time: 18 ms (61.16%), Space: 13.5 MB (70.52%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.cpp
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -8,28 +8,14 @@ public:
        return a;
    }
     int findMinDifference(vector<string>& time) {
-        // 0000=>2400
-        //,cmp);
-        // for(int i=0;i<time.size();i++)
-        // {
-        //     if(time[i]>"12:00")
-        //     {
-        //         int x=10*(time[i][0]-'0')+(time[i][1]-'0')-12;
-        //         time[i][0]='0'+x/10;
-        //         time[i][1]='0'+x%10;
-        //     }
-        // }
         sort(time.begin(),time.end());
         int ans= convert(time.back())-convert(time[0]);
-        // cout<<ans;
         ans=min(ans,1440-ans);
         for(int i=1;i<time.size();i++)
         {
              int a=convert(time[i])-convert(time[i-1]);
              ans=min(ans,min(a,1440-a));
         }
-        // for(auto x:time)
-        //     cout<<x<<" ";
         return ans;
     }
 };
