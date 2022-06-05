6efe5c8e614def5e5f23ed6fe2d9d0fcfb4b5763
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-05 18:08:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (59.76%) - LeetHub

--- 537-complex-number-multiplication/537-complex-number-multiplication.cpp
+++ 537-complex-number-multiplication/537-complex-number-multiplication.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
-    int convert(int &i, string &s)
-    {
-        int a=0,neg=1;
-        if(s[i]=='-')
-            i++,neg=-1;
-        while(i<s.size()&&(s[i]!='+'&&s[i]!='i'))
-            {
-                a*=10;
-                a+=s[i++]-'0';
-            }
-           a*=neg;
-        // cout<<a<<" ";
-        return a;
-    }
-    string complexNumberMultiply(string num1, string num2) {
-        //(a+bi)*(c+di)=(ac-bd)+(ad+bc)i
-        int a=0,b=0,c=0,d=0,i=0;
-         a=convert(i,num1);
-         i++;//+
-         b=convert(i,num1);
-         i=0;
-         c=convert(i,num2);
-         i++;
-         d=convert(i,num2);
-        
-         return to_string(a*c-b*d)+"+"+to_string(a*d+b*c)+"i";
-        
+    string complexNumberMultiply(string a, string b) {
+        int ra, ia, rb, ib;
+        char buff;
+        stringstream aa(a), bb(b), ans;
+        aa >> ra >> buff >> ia >> buff;
+        bb >> rb >> buff >> ib >> buff;
+        ans << ra*rb - ia*ib << "+" << ra*ib + rb*ia << "i";
+        return ans.str();
     }
 };
