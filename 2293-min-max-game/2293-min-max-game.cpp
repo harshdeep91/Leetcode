65254520db1501eca89655eb0a9761ea13886117
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-05 17:09:42 +0530

    Time: 11 ms (33.33%), Space: 9.9 MB (91.67%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -3,25 +3,11 @@ public:
     int minMaxGame(vector<int>& nums) {
         
         int k=nums.size();
-        while(k>1)
+       do
         {
-            
-            bool even=1;
-                for(int i=0;i<k/2;i++)
-            {
-                if(even)
-                nums[i]=min(nums[2*i],nums[2*i+1]);
-                else
-                 nums[i]=max(nums[2*i],nums[2*i+1]);
-                    even=!even;
-                    
-            }
-            // for(int i=0;i<k;i++)
-            //     cout<<nums[i]<<" ";
-            k/=2;
-            // cout<<k<<" "<<nums[0]<<endl;
-            // cout<<endl;
-        }
+            for(int i=0;i<k/2;i++)
+            nums[i]=i&1?nums[2*i]<nums[2*i+1]?nums[2*i+1]:nums[2*i]:nums[2*i]<nums[2*i+1]?nums[2*i]:nums[2*i+1];
+        } while(k/=2);
         
         return nums[0];
     }
