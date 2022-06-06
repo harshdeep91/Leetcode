490993d7afb78fd4299ff0b57f25cbb11f61e52f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-06 08:28:02 +0530

    Time: 27 ms (86.38%), Space: 22.4 MB (9.14%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int l=0,h=nums.size()-1;
-        while(l<=h)
-        {
-            int m=(l+h)>>1;
-            // cout<<m<<" "<<l<<" "<<h<<endl;
-            if(m+1<=h&&nums[m]==nums[m+1])
-            {
-                if((m-l)%2==0)
-                    l=m+2;
-                else
-                    h=m-1;
-            }
-            else if(m-1>=l&&nums[m]==nums[m-1])
-            {
-                // cout<<h-m<<" ";
-                if((h-m)%2==0)
-                    h=m-2;
-                else
-                    l=m+1;
-            }
-            else return nums[m];
+   int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                left = mid + 1;
+            else
+                right = mid;
         }
-        return 0;
+        
+        return nums[left];
     }
 };
