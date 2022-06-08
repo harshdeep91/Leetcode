6b540fcedb1a87471b72c068e56dd12a40f8d2ac
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-08 19:25:30 +0530

    Time: 52 ms (15.53%), Space: 13.7 MB (63.77%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,27 +1,31 @@
 class Solution {
+    private:
+    int find(int x, vector<int>& parents) {
+        return parents[x] == x ? x : find(parents[x], parents);
+    }
 public:
     int findCircleNum(vector<vector<int>>& M) {
-	int n=M.size(),ans=0;
-	if(n==0) return 0;
+        if (M.empty()) return 0;
+        int n = M.size();
 
-	vector<bool>vis(n,false);
+        vector<int> leads(n, 0);
+        for (int i = 0; i < n; i++) { leads[i] = i; }   // initialize leads for every kid as themselves
 
-	for(int i=0;i<n;i++)
-	{
-		if(!vis[i])
-		{
-			ans++;
-			dfs(M,vis,i);
-		}
-	}
-	return ans;
-}
+        int groups = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {   // avoid recalculate M[i][j], M[j][i]
+                if (M[i][j]) {
+                    int lead1 = find(i, leads);
+                    int lead2 = find(j, leads);
+                    if (lead1 != lead2) {       // if 2 group belongs 2 different leads, merge 2 group to 1
+                        leads[lead1] = lead2;
+                        groups--;
+                    }
+                }
+            }
+        }
+        return groups;
+    }
 
-void dfs(vector<vector<int>>& M, vector<bool>& vis, int i)
-{
-	vis[i]=true;
-	for(int j=0;j<M.size();j++)
-		if(M[i][j]==1 && !vis[j])
-			dfs(M,vis,j);
-}
+
 };
