b934cac87f348ae27452f18710d708139c188d5f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-09 19:03:53 +0530

    Time: 81 ms (5.27%), Space: 27.1 MB (52.19%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         // 2 4 
-        vector<int> tmp=nums;
-        sort(nums.begin(),nums.end());
-        int i=0,j=nums.size()-1;
-        while(i<j)
+        //check decreasing order if exist then find smallest index that is greater
+        stack<int> s;
+        int mi=nums.size(),mx=-1,prev,curr;
+        for(int i=0;i<nums.size();i++)
         {
-           if(tmp[i]==nums[i])
-               i++;
-           else if(tmp[j]==nums[j])
-               j--;
-            else 
-                return j-i+1;
+            curr=i;
+            if(!s.empty()&&nums[s.top()]>nums[i])
+                curr=s.top(),s.pop();
+            prev=curr;
+            while(!s.empty()&&nums[s.top()]>nums[i])
+                prev=s.top(),s.pop();
+            if(curr!=i&&nums[curr]>=nums[i])
+            {
+                mi=min(mi,prev);
+                mx=max(mx,i);
+                s.push(prev);
+                if(prev!=curr)
+                s.push(curr);
+            }
+            else
+            s.push(i);
+            // cout<<mi<<" "<<mx<<endl;
         }
-            return 0;
+            return mx==-1?0:mx-mi+1;
     }
 };
