40a9c7471367a418b2a2aecb93b47da9ae9da96a
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-09 16:02:11 +0530

    Time: 517 ms (14.54%), Space: 127.3 MB (22.94%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -10,15 +10,22 @@ public:
     }
     int arrayNesting(vector<int>& nums) {
         vector<int> adj[nums.size()],vis(nums.size(),0);
-        int count=0;
+        int count=0,mx=0,x;
         for(int i=0;i<nums.size();i++)
             adj[i].push_back(nums[i]);
         for(int i=0;i<nums.size();i++)
         {
-            if(!vis[nums[i]])
-                count=max(dfs(nums[i],nums,vis)+1,count);
+            count=0;
+            x=nums[i];
+            while(!vis[x])
+            {
+                count++;
+                vis[x]=1;
+                x=nums[x];
+            }
+            mx=max(count,mx);
             
         }
-     return count;   
+       return mx;   
     }
 };
