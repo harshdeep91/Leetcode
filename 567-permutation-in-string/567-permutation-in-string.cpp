135a684478a0d689c6bcb6c783051b0750f8e6d1
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-09 17:14:42 +0530

    Time: 7 ms (84.64%), Space: 7.3 MB (75.54%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -4,22 +4,17 @@ public:
         //first check every permutation 
         // second approach we only need continuos count 
         if(s1.size()>s2.size())return 0;
-        int a[26]={0};
-        for(int i=0;i<s1.size();i++)
-            a[s1[i]-'a']++;
-        int j=0,i=0,k=0;
-        
+        int j=0,i=0,k=0,a[26]={0};
+        while(i<s1.size())
+            a[s1[i++]-'a']++;
         while(j<s1.size()-1)
-        {
             a[s2[j++]-'a']--;
-        }
-        
+        i=0;
         while(j<s2.size())
         {
-               a[s2[j++]-'a']--;
-                k=0;
-            while(k<26&&!a[k])k++;
-            if(k==26)return 1;
+              a[s2[j++]-'a']--;
+            for(k=0;k<26&&!a[k];k++);
+            if(k==26) return 1;
              a[s2[i++]-'a']++;
         }
         return 0;
