2cea767efd5eaf15b633064f51ae5dddeb54b047
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-10 17:09:09 +0530

    Time: 210 ms (41.55%), Space: 12.9 MB (52.91%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+        //for one to be a traingle it should follow a+b<c for all side
+        //if we sort we only need to check big side with smaller one if that is correct then all is correct
+        // now  choose i first then select j and k carefully
+        //if nums[i]+nums[j]>nums[k] it means all before k will follow traingle properties so we increase k count to get last traingle
+        // if nums[i]+nums[j]<=nums[k] it means here we need to calculate all possible traingles [j,k) => k-j-1 (-1 because of increase of k)
+        //now increase j 
+        //after finishing k==n we need to again count all possible traingle that can be made
+        // 2,3,4,4 in this example i=0,j=1,k=4 now k-j++ will give 2 but it should be 3 because 2,4,4 is not counted
+        
         sort(nums.begin(),nums.end());
         int n=nums.size(),count=0,k,j;
         for(int i=0;i<n-2;i++)
@@ -12,19 +21,13 @@ public:
                 if(j>=k||nums[i]+nums[j]>nums[k])
                     k++;
                 else
-                {
-                    j++;
-                    // count+= ((k-j)*(k-j+1))/2;
-                    count+=k-j;
-                }
+                    count+=k- ++j;
             }
             while(j<k)
-            {
-                    j++;
-                    count+=k-j;
-            }
+                    count+=k- ++j;
         }
         // [6,16,24,24,29,44]
         return count;
     }
+    
 };
