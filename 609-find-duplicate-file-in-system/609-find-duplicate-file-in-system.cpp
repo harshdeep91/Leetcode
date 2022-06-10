38b26b9c606d4c0430b5a42e3c37c360fe046bf8
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-10 09:39:39 +0530

    Time: 195 ms (25.08%), Space: 44.5 MB (29.26%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,41 +1,26 @@
 class Solution {
-    unordered_map<string,vector<string>> m;
 public:
-    void dir(int &j,string path,string &t)
-    {
-        path.push_back('/');
-        while(t[j]!='(')
-        {
-            path.push_back(t[j++]);
-        }
-        j++;
-        string l; 
-        while(t[j]!=')')
-            l.push_back(t[j++]);
-        j++;
-        m[l].push_back(path);
-        if(j<t.size()&&t[j]==' ')j++;
-        // cout<<path<<" "<<l<<endl;
+   vector<vector<string>> findDuplicate(vector<string>& paths) {
+    unordered_map<string, vector<string>> files;
+    vector<vector<string>> result;
+
+    for (auto path : paths) {
+	    stringstream ss(path);
+	    string root;
+	    string s;
+	    getline(ss, root, ' ');
+	    while (getline(ss, s, ' ')) {
+		    string fileName = root + '/' + s.substr(0, s.find('('));
+		    string fileContent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
+		    files[fileContent].push_back(fileName);
+	    }
     }
-    vector<vector<string>> findDuplicate(vector<string>& paths) {
-        //find content
-        m.clear();
-        int i=0;
-        while(i<paths.size())
-        {
-            int j=0;
-            string path;    
-            while(paths[i][j]!=' ')
-                path.push_back(paths[i][j++]);
-            j++;
-            while(j<paths[i].size())
-                dir(j,path,paths[i]);
-            i++;
-        }
-        vector<vector<string>> ans;
-        for(auto x:m)
-            if(x.second.size()>1)
-          ans.push_back(x.second);
-        return ans;
+
+    for (auto file : files) {
+	    if (file.second.size() > 1)
+		    result.push_back(file.second);
     }
+
+    return result;
+}
 };
