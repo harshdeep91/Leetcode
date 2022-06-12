717b3ba29fa832e23ddfeea4e6708bb0533de452
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-12 17:42:38 +0530

    Time: 326 ms (78.74%), Space: 89.2 MB (97.81%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        unordered_set<int> s;
+        vector<bool> v(10001,0);
         int i=0,j=0,mx=0,sum=0;
         while(j<nums.size())
         {
             sum+=nums[j];
-            if(s.count(nums[j]))//if exist then remove it
+            if(v[nums[j]])//if exist then remove it
             {
                  while(nums[i]!=nums[j])
                  {
-                     s.erase(nums[i]);
+                     v[nums[i]]=0;
                      sum-=nums[i++];
                  }
-                    s.erase(nums[i]);
+                     v[nums[i]]=0;
                      sum-=nums[i++];
             }
-            s.insert(nums[j++]);
+            v[nums[j++]]=1;
             mx=max(sum,mx);
         }
         return mx;
