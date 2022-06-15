ff6517a4a614035796144d873157216640cbbde2
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-15 09:14:24 +0530

    Time: 52 ms (89.68%), Space: 50.2 MB (65.76%) - LeetHub

--- 648-replace-words/648-replace-words.cpp
+++ 648-replace-words/648-replace-words.cpp
@@ -1,25 +1,53 @@
-class Solution {
+class TrieNode{
 public:
-    string replaceWords(vector<string>& d, string s) {
-        //take only those word which have less value
-        unordered_set<string> m;
-        for(auto x:d)m.insert(x);
-        int i=0 , up=0;
-        string ans;
-        while(i<s.size())
-        {
-             string t;
-             up=0;
-             while(i<s.size()&&s[i]!=' ')
-             {
-                 t.push_back(s[i++]);
-                 if(m.find(t)!=m.end())
-                     while(i<s.size()&&s[i]!=' ')i++;
-             }
-            if(i<s.size())
-            t.push_back(s[i++]);
-            ans+=t;
+    TrieNode *child[26];
+    bool isEnd;
+    
+    TrieNode(){
+        this->isEnd = false;
+        for(int i=0; i<26; i++) this->child[i]= NULL;
+    }
+};
+
+class Solution {
+    TrieNode *newNode;
+public:
+    string check(string word){
+        TrieNode *t = newNode;
+        string s = "";
+        
+        for(auto i : word){
+            if(!t->child[i-'a']) break;
+            s += i;
+            t = t->child[i-'a'];
+            if(t->isEnd) return s;
         }
+        return word;
+    }
+    
+    void insert(string word){
+        TrieNode *temp = newNode;
+        
+        for(auto l : word){
+            if(!temp->child[l-'a']) temp->child[l-'a'] = new TrieNode();
+            temp = temp->child[l-'a'];
+        }
+        
+        temp->isEnd = true;
+    }
+    
+    string replaceWords(vector<string>& dict, string sentence) {
+        newNode = new TrieNode();
+        for(auto s : dict) insert(s);
+        istringstream ss(sentence);
+        string word = "", ans="";
+        
+        for(; ss>>word; ){
+            ans += check(word);
+            ans += ' ';
+        }
+        ans.pop_back();
+        
         return ans;
     }
 };
