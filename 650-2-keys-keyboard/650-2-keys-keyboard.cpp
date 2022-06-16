7c40238a3862661a8d5a7660131bbe6ab5574665
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-16 08:32:37 +0530

    Time: 6 ms (65.10%), Space: 5.9 MB (71.85%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     int minSteps(int n) {
-        //find min factor multiply it with 2 check if divisible or not;
+        //find min factor multiply it with 2 or 3 check if divisible or not;
+        
         int count=0,fac=1;
-        if(n==1)return 0;
-        // for(int i=2;i<=n;i++)            
-        // {
-        //     if(n%i==0)
-        //     {
-        //        fac=i;
-        //        break; 
-        //     }
-        // }
-        // //1 ,fac are the factors
-        // count=fac;
-        // cout<<fac<<" ";
         while(fac<n)
         {
             for(int i=2;i*fac<=n;i++)
@@ -27,19 +16,7 @@ public:
                     break;
                 }
             }
-        }
-//         for(int i=fac*2;i<=n-1;i*=2)
-//         {
-//             if(n%i==0)
-//             {
-                
-//                 count+=i/fac;
-//                 fac=i;
-//                 
-//             }
-//         }
-//          if(fac!=n)
-//            count+=n/fac;
+        }      
         return count;
     }
 };
