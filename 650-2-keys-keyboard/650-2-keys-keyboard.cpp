b97db7301e2e31636199da31bcab9a5f1a646e23
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-16 08:35:28 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.59%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int minSteps(int n) {
         //find min factor multiply it with 2 or 3 check if divisible or not;
+        //for 24 we can get less amount of value only if we continue in factors
+        //because this will help to use full copy (means doubling of numbers)
+        //so 24=>[1,2,3,4,6,8,12,24]=>[1,2,4,8,24]=> this pattern gives less answer
         
         int count=0,fac=1;
         while(fac<n)
