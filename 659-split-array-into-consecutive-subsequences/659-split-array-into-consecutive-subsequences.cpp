d0d362ce99fb9b92967cb21ca85b0b2400706ce4
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-18 08:31:20 +0530

    Time: 104 ms (91.90%), Space: 54.6 MB (90.58%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-   bool isPossible(vector<int>& nums) {
-        unordered_map<int,int> cnt, tails;
-        for(int &i : nums) cnt[i]++;
-        for(int &i : nums){
-            if(!cnt[i]) continue;
-            cnt[i]--;
-            if(tails[i-1] > 0){
-                tails[i-1]--;
-                tails[i]++;
+    struct Compare {
+        bool operator()(array<int, 2> subsequence1, array<int, 2> subsequence2) {
+            if (subsequence1[1] == subsequence2[1]) {
+                return (subsequence1[1] - subsequence1[0]) > (subsequence2[1] - subsequence2[0]);
             }
-            else if(cnt[i+1] && cnt[i+2]){
-                cnt[i+1]--;
-                cnt[i+2]--;
-                tails[i+2]++;
+            return subsequence1[1] > subsequence2[1];
+        }
+    };
+
+    bool isPossible(vector<int> &nums) {
+        priority_queue<array<int, 2>, vector<array<int, 2>>, Compare> subsequences;
+
+        for (int num : nums) {
+            //Condition 1 - remove non qualifying subsequences
+            while (!subsequences.empty() && subsequences.top()[1] + 1 < num) {
+                array<int, 2> currentSubsequence = subsequences.top();
+                subsequences.pop();
+                int subsequenceLength = currentSubsequence[1] - currentSubsequence[0] + 1;
+                if (subsequenceLength < 3) {
+                    return false;
+                }
             }
-            else return false;
+            //Condition 2 - create a new subsequence
+            if (subsequences.empty() || subsequences.top()[1] == num) {
+                subsequences.push({num, num});
+            } else {
+                //Condition 3 - add num to an existing subsequence
+                array<int, 2> currentSubsequence = subsequences.top();
+                subsequences.pop();
+                subsequences.push({currentSubsequence[0], num});
+            }
+        }
+
+        //If any subsequence is of length less than 3 return false
+        while (!subsequences.empty()) {
+            array<int, 2> currentSubsequence = subsequences.top();
+            subsequences.pop();
+            int subsequenceLength = currentSubsequence[1] - currentSubsequence[0] + 1;
+            if (subsequenceLength < 3) {
+                return false;
+            }
         }
         return true;
     }
