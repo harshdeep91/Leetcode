cad163580dc4886513045f81f6aee345884bd8da
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-18 20:55:15 +0530

    Time: 8 ms (86.21%), Space: 17.2 MB (85.63%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,31 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        // using indexing 2*i+1 for left child and 2*i+2 for right child
-        queue<pair<TreeNode*,int>>q;
+        queue<pair<TreeNode*,int>> q;
         q.push({root,0});
-        int ans=0;
-        while(!q.empty()){
-            int n=q.size();
-            int start=q.front().second;
-            int end=q.back().second;
-            ans=max(ans,end-start+1);
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front().first;
-                int p=q.front().second;
+        long long  mx=0,l,r,size,dis;
+        while(!q.empty())
+        {
+             size=q.size(),l=INT_MAX,r=INT_MIN;
+            while(size--)
+            {
+                TreeNode* p=q.front().first;
+                dis=q.front().second;
+                l=min(l,dis);
+                r=max(r,dis);
                 q.pop();
-                if(node->left!=NULL){
-                    q.push({node->left,(long long)2*p+1});
-                }
-                if(node->right!=NULL){
-                    q.push({node->right,(long long)2*p+2});
-                }
-                
-                
+                if(p->left)
+                    q.push({p->left,(long long)2*dis+1});
+                if(p->right)
+                    q.push({p->right,(long long)2*dis+2});
+                mx=max(mx,r-l+1);
             }
-        
         }
-        return ans;
-        
+        return mx;
     }
 };
