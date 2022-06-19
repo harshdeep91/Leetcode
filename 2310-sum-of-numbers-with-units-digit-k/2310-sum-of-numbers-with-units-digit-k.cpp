e015e91246d64e99b60aae582850a15053bfaf80
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-19 17:20:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.33%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    long solve(vector<int>&nums,int t,int i,int n,vector<vector<long>>&dp){
-        if(t==0) return 0;
-        if(t<0 || i>=n) return INT_MAX;
-        if(dp[i][t]!=-1) return dp[i][t];
-        long c1 = 1+solve(nums,t-nums[i],i,n,dp);
-        long c2 = solve(nums,t,i+1,n,dp);
-        return dp[i][t] = min(c1,c2);
-    }
     int minimumNumbers(int num, int k) {
-        int x = k;
-        vector<int>nums;
-        while(x<=num){
-            if(x!=0) nums.push_back(x);
-            x = (10+x);
+        if(num==0) return 0;
+        
+        int unit=num%10;
+        int a=k;
+        
+        bool possible=0;
+        int count=0;
+        for(int i=0; i<10; i++)
+        {
+            
+            count++;
+            if(a==unit) {
+                possible=1;
+                break;
+            }
+            a+=k;
+            a%=10;
         }
-        vector<vector<long>>dp(nums.size()+1,vector<long>(num+1,-1));
-        int c = solve(nums,num,0,nums.size(),dp);
-        return (c>=INT_MAX)?-1:c;
+       cout<<possible;
+        
+        if(!possible) return -1;
+        if(num<count*k) return -1;
+        else 
+            return count;
+        
+        
     }
 };
