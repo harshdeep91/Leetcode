3785c9bdfc38fc17bac81d4e17f8ba86a4bfb233
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-20 16:36:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (6.61%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.cpp
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -5,16 +5,16 @@ public:
         //2 7 3 6 value  
         //find max value from right and its index
         // subtract and add by 10th multiplication
-        int mx=num,count=0,index=0,value=0,k,tmp=num;
+        int mx=num,count=0,index=0,value=0,k,tmp=num,place1,place2,t;
         while(num)
         {
             k=num%10;
             num/=10;
             if(k<value)//that means perform swapping
             {
-                int place1 = pow(10,count);
-                int place2 = pow(10,index);
-                int t=tmp;
+                place1 = pow(10,count);
+                place2 = pow(10,index);
+                t=tmp;
                 t-= (k*place1+value*place2);
                 t+= (value*place1+k*place2);  //second digit remove
                 mx=max(t,mx);
