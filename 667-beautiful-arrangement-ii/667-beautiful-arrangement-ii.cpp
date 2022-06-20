ec0b406a33d52b02ff811830e028444326d44d90
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-20 16:06:55 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (21.95%) - LeetHub

--- 667-beautiful-arrangement-ii/667-beautiful-arrangement-ii.cpp
+++ 667-beautiful-arrangement-ii/667-beautiful-arrangement-ii.cpp
@@ -1,28 +1,9 @@
 class Solution {
 public:
-    vector<int> constructArray(int n, int k) {
-        vector<int> v;
-        unordered_set<int> m;
-        v.push_back(1);
-        m.insert(1);
-        bool flag=1; //add k
-        while(k)
-        {
-            if(flag)
-            v.push_back(v.back()+k);
-                else v.push_back(abs(v.back()-k));
-            m.insert(v.back());
-            k--;
-            flag=!flag;
-        }
-        int count=v.back()+1;
-        while(m.find(count)!=m.end())
-            count++;
-        while(v.size()<n)
-        {
-            v.push_back(count);
-            count++;
-        }
-        return v;
+   vector<int> constructArray(int n, int k) {
+        vector<int> res;
+        for (int i = 1, j = n; i <= j; )
+            res.push_back(k > 1 ? (k-- % 2 ? i++ : j--) : i++);
+        return res;
     }
 };
