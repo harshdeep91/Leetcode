eec1aeb8cb8bb066d36c55e1b39bbf09f475a9db
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-20 09:12:33 +0530

    Time: 45 ms (96.04%), Space: 13.7 MB (96.04%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,45 +1,10 @@
-class trie{
-    
-   trie *child[26]={0};
-    int wordcount=0;
-    public:
-    void insert(string &s)
-    {
-        if(this->child[s[0]-'a']==0)
-        this->child[s[0]-'a']=new trie();
-        trie *p=this->child[s[0]-'a'];
-         p->wordcount++;
-        for(int i=1;i<s.size();i++)
-        {
-            if(!p->child[s[i]-'a'])
-                p->child[s[i]-'a']=new trie();
-            p=p->child[s[i]-'a'];
-            p->wordcount++;
-        }
-    }
-    //count only if wordcount==0 because this means we dont have any merging suffix linke men,time  => if(me,time )<=this is mearging
-    int count(string &s)
-    {
-        trie *p=this->child[s[0]-'a'];//this means we are jumping to first char
-        for(int i=1;i<s.size();i++)
-            p=p->child[s[i]-'a'];
-         p->wordcount--;
-        return p->wordcount==0?s.size()+1:0;
-    }
-};
+
 class Solution {
-    trie *first=0;
 public:
-    int minimumLengthEncoding(vector<string>& words) {
-        first=new trie();
-        for(int i=0;i<words.size();i++)
-        {
-            reverse(words[i].begin(),words[i].end());
-            first->insert(words[i]);
-        }
-        int ans=0;
-         for(int i=0;i<words.size();i++)
-            ans+=first->count(words[i]);
-        return ans;
+  int minimumLengthEncoding(vector<string>& ws, int res = 0) {
+    for (auto i = 0; i < ws.size(); ++i) reverse(ws[i].begin(), ws[i].end());
+    sort(ws.begin(), ws.end());
+    for (auto i = 0; i < ws.size() - 1; ++i) res += ws[i] == ws[i + 1].substr(0, ws[i].size()) ? 0 : ws[i].size() + 1;
+        return res + ws[ws.size() - 1].size() + 1;
     }
 };
