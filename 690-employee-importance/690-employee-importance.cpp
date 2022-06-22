7eb77b853fe962eb9d521943ca3272e6d7676626
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-22 18:45:54 +0530

    Time: 70 ms (18.84%), Space: 15.1 MB (34.35%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -15,16 +15,9 @@ public:
     {
         int val=m[i][0];
         for(int j=1;j<m[i].size();j++)
-        {
             val+=dfs(m[i][j]);
-        }
-        // cout<<i+1<<" "<<val<<endl;
         return val;
     }
-    // static bool cmp(Employee *a ,Employee *b)
-    // {
-    //     return a->id<b->id;
-    // }
     int getImportance(vector<Employee*> e, int id) {
         //find the id first
         for(auto x:e)
@@ -33,9 +26,6 @@ public:
             for(auto y:x->subordinates)
                 m[x->id].push_back(y);
         }
-        // vector<bool> vis(e.size()+1,0);
-        // for(auto x:employees)
-        
         return dfs(id);
     }
 };
