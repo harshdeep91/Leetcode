f9ec462fbaba7c87e061e516397ca0955bf647ea
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-23 17:30:39 +0530

    Time: 395 ms (11.59%), Space: 94.8 MB (21.87%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,11 +1,39 @@
 class Solution {
+    vector<vector<int>> dp;
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-       int cash = 0, hold = -prices[0];
-        for (int i = 1; i < prices.size(); i++) {
-            cash = max(cash, hold + prices[i] - fee);
-            hold = max(hold, cash - prices[i]);
+    int solve(int i,int buy,int &fee,vector<int> &prices)
+    {
+        //buy or not buy
+        if(i==prices.size())return 0;
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        if(buy)
+        {
+            return dp[i][buy]=max(-prices[i]+solve(i+1,0,fee,prices),solve(i+1,1,fee,prices));
         }
-        return cash;
+        else
+        {
+            return dp[i][buy]=max(prices[i]-fee+solve(i+1,1,fee,prices),solve(i+1,0,fee,prices));
+        }
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        dp.resize(prices.size(),vector<int>(2,-1));
+        //  int prevbuy=0,prevnotbuy=0,currbuy=0,currnotbuy=0;
+        // for(int i=prices.size()-1;i>=0;i--)
+        // {
+        //     for(int buy=0;buy<=1;buy++)
+        //     {
+        //         int profit=0;
+        //         if(buy==1)//means we need to buy here by either not buying or if buying
+        //         {
+        //             profit+=max(-prices[i]+prevnotbuy,prevnotbuy);
+        //         }
+        //         else
+        //         {
+        //             profit+= max(prices[i]-fee+prevbuy,prevbuy);
+        //         }
+        //         currbuy=prevbuy
+        //     }
+        // }
+        return solve(0,1,fee,prices);
     }
 };
