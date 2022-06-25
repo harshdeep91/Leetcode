907d8c0a6acb0f1c7e17c7f9ff96d6ed755b47e7
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-25 18:45:01 +0530

    Time: 443 ms (22.05%), Space: 64.4 MB (12.37%) - LeetHub

--- 756-pyramid-transition-matrix/756-pyramid-transition-matrix.cpp
+++ 756-pyramid-transition-matrix/756-pyramid-transition-matrix.cpp
@@ -8,11 +8,11 @@ public:
         if(bottom.size()==1)return 1;
         if(top.size()+1==bottom.size())//then increase level
             return solve(0,"",top);
-        string key=top+bottom;
+        string key=top+bottom,prefix;
         if(vis.find(key)!=vis.end())return vis[key];
         //find first two digit and put the char one by one
-        string prefix=bottom.substr(i,2);
-        bool one=0;
+         prefix.push_back(bottom[i]);
+         prefix.push_back(bottom[i+1]);
         for(auto x:m[prefix])
         {
             top.push_back(x);
@@ -28,9 +28,14 @@ public:
         //find every three digit allowed 
         //check one by one
         //put the every possible triangle and check
+        string t;
         for(auto x:allowed)
         {
-            m[x.substr(0,2)].push_back(x[2]);
+            t.push_back(x[0]);
+            t.push_back(x[1]);
+            m[t].push_back(x[2]);
+            t.pop_back();
+            t.pop_back();
         }
         return solve(0,"",bottom);
     }
