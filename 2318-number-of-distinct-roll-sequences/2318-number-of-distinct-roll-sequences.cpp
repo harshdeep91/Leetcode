4ca4d7967b566ea9715ac0810a996915ac6e30d3
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-26 17:22:38 +0530

    Time: 315 ms (100.00%), Space: 8.8 MB (100.00%) - LeetHub

---
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  int dp[10001][7][7] = {};
+    public:
+int distinctSequences(int n, int p = 0, int pp = 0) {
+    if (n == 0)
+        return 1;
+    if (dp[n][p][pp] == 0)
+        for (int d = 1; d < 7; ++d)
+            if (d != p && d != pp && (p == 0 || gcd(d, p) == 1))
+                dp[n][p][pp] = (dp[n][p][pp] + distinctSequences(n - 1, d, p)) % 1000000007;
+    return dp[n][p][pp];
+}
+};
