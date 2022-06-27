513e22d3528b5e85f04e3074475b2f914e77d621
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-27 18:45:01 +0530

    Time: 10 ms (26.04%), Space: 6.8 MB (48.39%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int a[256]={0},partition=1;
-        vector<int> v,size;
-        for(int i=0;i<s.size();i++)
-        {
-            // if(a[s[i]]==0)//never encountered before
-            // {
-            // }
-            if(a[s[i]]!=0) //encountered before update every thing to same partition
-            {
-                
-                partition=a[s[i]];
-                // cout<<i<<" "<<s[i]<<" "<<partition<<" "<<a[s[size.back()]]<<" "<<size.back()<<" ";
-                while(!size.empty()&&a[s[size.back()]]>=partition)
-                    size.pop_back();
-                
-                 for(int j='a';j<='z';j++)
-                     if(a[j]>partition)
-                         a[j]=partition;
+        unordered_map<char,int>mp;
+        // filling impact of character's
+        for(int i = 0; i < s.size(); i++){
+            char ch = s[i];
+            mp[ch] = i;
+        }
+        // making of result
+        vector<int> res;
+        int prev = -1;
+        int maxi = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            maxi = max(maxi, mp[s[i]]);
+            if(maxi == i){
+                // partition time
+                res.push_back(maxi - prev);
+                prev = maxi;
             }
-            a[s[i]]=partition++;
-            size.push_back(i);
-            // cout<<size.back()<<endl;
         }
-        int last=-1;
-        // cout<<endl<<endl;
-        for(int i=0;i<size.size();i++)
-        {
-            // cout<<size[i]<<" ";
-            v.push_back(size[i]-last);
-            last=size[i];
-        }
-        // cout<<endl;
-        return v;
+        return res;
     }
 };
