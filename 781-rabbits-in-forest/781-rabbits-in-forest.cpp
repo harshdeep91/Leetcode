8f3c84f9a571b345752ec3640e7c23ee056c77cf
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-28 18:10:23 +0530

    Time: 8 ms (44.09%), Space: 8.7 MB (18.40%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.cpp
+++ 781-rabbits-in-forest/781-rabbits-in-forest.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int numRabbits(vector<int>& a) {
-        //new solving pattern
-        //think in 5 min 
-        // if not use 1st hint 5 min
-        //  use second hint 5 min
-        //solve for 5 min
-        // if not stop see ans
-        //only 20 min give
-        sort(a.begin(),a.end());
-        int count=0,i=0;
-        while(i<a.size())
-        {
-           //said the 1 rabbit then we will check next value
-                int j=1;//means next rabbits must be of same answer
-                count+= a[i]+1;
-                while(j<=a[i])
-                {
-                    if(i+j<a.size()&&a[i+j]==a[i])
-                    {
-                        j++;
-                        // count++;
-                    }
-                    else break;
-                }
-              // cout<<i<<" "<<j<<" "<<count<<endl;
-                i=i+j;
+    int numRabbits(vector<int>& answers) {
+        if(answers.empty()) return 0;
+        unordered_map<int,int> m;
+        int res=0;
+        for(int a:answers){
+            if(a==0) res++;
+            else{
+                if(m[a]==0) res+=a+1;
+                m[a]++;
+                if(m[a]==a+1) m[a]=0;
+            }           
         }
-        return count;   
+        return res;
     }
 };
