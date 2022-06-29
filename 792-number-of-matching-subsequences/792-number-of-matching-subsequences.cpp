6f498ab2f6bf0abe5e2541f1fb5406b46464b961
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-29 10:29:48 +0530

    Time: 237 ms (78.31%), Space: 36.2 MB (90.00%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-     bool matchString(string a,string b){
-        int size1 = a.size();
-        int size2 = b.size();
-        int i=0,j=0;
-        for(i=0,j=0; i<size1 && j < size2 ;i++){
-            if(a[i]==b[j]){
-                 j++;
-            }
-        }
-        return j==size2;
-    }
     int numMatchingSubseq(string s, vector<string>& words) {
-        int n=words.size();
-        unordered_map<string,int>mp;
-        int ans=0;
-        for(auto& x:words){
-            if(mp.find(x)!=mp.end()) ans+=mp[x];
-            else {
-                mp[x]=matchString(s,x);
-                ans+=mp[x];
+        int ans = 0;
+        int l1 = s.length(); //declare the length of main string
+        map<string,int> m;  // declare a map to store the results of subsquences already checked
+        for (auto word: words){
+            int l2 = word.length(); // declare length of current word
+            int j = 0;
+            if (m.find(word)==m.end()){ // Check for subsequence if current word not found in map
+               for (int i=0;i<l1 && j<l2;i++){   // if we reach end of current word, it means subsequence found. Hence no need to check further
+                    if (s[i]==word[j]){
+                        j++;
+                    }
+                }
+                ans+=(j==l2); // check if we reach end of the word 
+                m[word] = (j==l2);
             }
+            else{
+                ans+= m[word]; // If word is already checked before, no need to check again
+            }
+            
+            
         }
-        
         return ans;
     }
 };
