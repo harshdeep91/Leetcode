777f87ae80d3ae38211b9c615af2ab21e8d6deb0
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-29 18:43:11 +0530

    Time: 32 ms (5.88%), Space: 48.3 MB (5.33%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
@@ -2,7 +2,7 @@ class Solution {
     int size,m=pow(10,9)+7;
     vector<vector<int>> dp;
 public:
-    int solve(int i,int j,string s)
+    int solve(int i,int j)
     {
         // cout<<i<<" "<<j<<" "<<count<<endl;
         if(i==size&&j==size)
@@ -12,25 +12,25 @@ public:
         long long x=0;
              if(i==j)
              {
-                 x+=solve(i+2,j+2,s+"12")%m;//--
-                 x+=solve(i+1,j+1,s+"3")%m;// |
-                 x+= solve(i+1,j+2,s+"5")%m; // |_
-                 x+= solve(i+2,j+1,s+"7")%m;//|^
+                 x+=solve(i+2,j+2)%m;//--
+                 x+=solve(i+1,j+1)%m;// |
+                 x+= solve(i+1,j+2)%m; // |_
+                 x+= solve(i+2,j+1)%m;//|^
              }
              else if(abs(i-j)==1)
              {
                  if(i-j==1)
-                 x+= solve(i+1,j+2,s+"4")%m;// _| 
+                 x+= solve(i+1,j+2)%m;// _| 
                  else
-                     x+= solve(i+2,j+1,s+"6")%m;// ^| 
-                 x+= solve(i+2,j,s+"1")%m+solve(i,j+2,s+"2")%m;
+                     x+= solve(i+2,j+1)%m;// ^| 
+                 x+= solve(i+2,j)%m+solve(i,j+2)%m;
              }
         return dp[i][j]=x%m;
     }
     int numTilings(int n) {
         size=n;
         dp.resize(n+1,vector<int>(n+1,-1));
-        return solve(0,0,"");
+        return solve(0,0);
             
     }
 };
