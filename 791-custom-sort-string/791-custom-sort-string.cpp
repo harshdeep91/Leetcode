df7ea423b1e98c4339a0f488995ce4cf9ee77b29
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-29 10:03:25 +0530

    Time: 6 ms (19.71%), Space: 6.3 MB (57.54%) - LeetHub

---
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -0,0 +1,40 @@
+// class cmp{
+//     public:
+//     bool operator()(char x,char y)
+//     {
+//         return x<y;
+//     }
+// };
+
+
+class Solution {
+    // static int a[26];
+public:
+    // static bool cmp(char &x,char &y){
+    //     return a[x]<a[y];
+    // }
+    string customSortString(string order, string s) {
+        int a[26];
+        for(int i=0;i<26;i++)a[i]=201;
+        for(int i=0;i<order.size();i++)
+        {
+            if(a[order[i]-'a']==201)
+            {
+                a[order[i]-'a']=i;
+                // cout<<order[i]<<" "<<i<<endl;
+            }
+        }
+        priority_queue<pair<int,char>,vector<pair<int,char>>,greater<pair<int,char>>>q;
+        for(auto x:s)
+        {
+            q.push({a[x-'a'],x});
+        }
+        s.clear();
+        while(!q.empty())
+        {
+            s.push_back(q.top().second);
+            q.pop();
+        }
+        return s;
+    }
+};
