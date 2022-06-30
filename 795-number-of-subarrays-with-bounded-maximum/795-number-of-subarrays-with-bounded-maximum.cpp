443eee54c6d7fd08962e97331c1f12a3a07d8ebd
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-30 18:05:26 +0530

    Time: 83 ms (80.54%), Space: 52.3 MB (29.11%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
@@ -3,38 +3,28 @@ public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
     //if max ith element is out of range then we
     //start new  array from i+1
-    int count=0,i=0,j=0,mx=0,prev=0;
-    while(j<nums.size())
-    {
-        mx=max(nums[j],mx);
-        //for low value that are out of range we need some type of counter 
-        if(mx>=left&&mx<=right)//in range
+        int count=0,i=0,j=0,mx=0,prev=0;
+        while(j<nums.size())
         {
-            if(nums[j]>=left&&nums[j]<=right)//in range add new array
+            mx=max(nums[j],mx);
+            //for low value that are out of range we need some type of counter 
+            if(mx>=left&&mx<=right)//in range
             {
-                count+= j-i+1;
-                prev=j-i+1;
+                if(nums[j]>=left&&nums[j]<=right)//in range add new array
+                     prev=j-i+1;
+                    count+=prev; //because of case 4 5 2 3 range 4 to 5 we cant add [2 3] and [2],[3]
             }
             else 
-                count+=prev;
-            // cout<<mx<<" "<<nums[j]<<" "<<count<<endl;
+            {
+                if(nums[j]>right)
+                  i=j+1;
+                else if(nums[i]>right)//they could be included 5 6 7 range 7 to 7
+                    i=j;
+                mx=0;
+                prev=0;
+            }
+            j++;
         }
-        else if(nums[j]<=right)//they could be included 5 6 7 range 7 to 7
-        {
-          // i=j+1;
-            mx=0;
-            prev=0;
-            if(nums[i]>right)//then dont include new value;
-                i=j;
-        }
-        else 
-        {
-            i=j+1;
-            mx=0;
-            prev=0;
-        }
-        j++;
-    }
-    return count;
+        return count;
     }
 };
