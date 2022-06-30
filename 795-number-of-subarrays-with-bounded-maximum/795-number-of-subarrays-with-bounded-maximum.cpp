a3fffdbfb87a22b39edc1911c4e5a9763a8f4e85
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-06-30 18:10:59 +0530

    Time: 133 ms (23.10%), Space: 52.2 MB (70.09%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-    //if max ith element is out of range then we
-    //start new  array from i+1
-        int count=0,i=0,j=0,mx=0,prev=0;
-        while(j<nums.size())
-        {
-            mx=max(nums[j],mx);
-            //for low value that are out of range we need some type of counter 
-            if(mx>=left&&mx<=right)//in range
-            {
-                if(nums[j]>=left&&nums[j]<=right)//in range add new array
-                     prev=j-i+1;
-                    count+=prev; //because of case 4 5 2 3 range 4 to 5 we cant add [2 3] and [2],[3]
-            }
-            else 
-            {
-                if(nums[j]>right)
-                  i=j+1;
-                else if(nums[i]>right)//they could be included 5 6 7 range 7 to 7
-                    i=j;
-                mx=0;
-                prev=0;
-            }
-            j++;
+    int numSubarrayBoundedMax(vector<int>& A, int L, int R) {
+        int result=0, left=-1, right=-1;
+        for (int i=0; i<A.size(); i++) {
+            if (A[i]>R) left=i;
+            if (A[i]>=L) right=i;
+            result+=right-left;
         }
-        return count;
+        return result;
     }
 };
