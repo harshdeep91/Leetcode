0fa99f5994f3959b870084fa2bd3b162523941fe
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-01 09:02:15 +0530

    Time: 67 ms (57.42%), Space: 17 MB (40.06%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a,vector<int> &b)
-    {
-        return a[1]>b[1];
+    static bool myfunction(vector<int>& a, vector<int>& b){
+        return a[1] > b[1];
     }
-    int maximumUnits(vector<vector<int>>& b, int t) {
-       //this is not a knapsack because each box has no that can be breakable
-        sort(b.begin(),b.end(),cmp);
-        // for(auto x:b)
-        //     cout<<x[0]<<" "<<x[1]<<endl;
-        int ans=0,i=0;
-        while(t--&&i<b.size())
-        {
-            if(b[i][0]>0)
-            {
-                ans+=b[i][1];
-                b[i][0]--;
-            }
-            else
-                i++,t++;
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+	    //custom sort (in increasing order of numberOfUnitsPerBox as we have to return  maximum total number of units )
+        sort(boxTypes.begin(),boxTypes.end(),myfunction);
+		//greedily pick boxes till capacity is full
+        int ans=0;
+        for(auto box: boxTypes){
+            int x=min(box[0],truckSize);  //choose minimum boxes from available boxes and capacity left
+            ans+=(x*box[1]);  //adding units in ans
+            truckSize-=x;  //reduce the capacity
+            if(!truckSize) break;  //capacity full
         }
         return ans;
     }
