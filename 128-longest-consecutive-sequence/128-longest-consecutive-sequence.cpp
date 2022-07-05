94057ebd7638c78d72673ee8e289fc074cfc7318
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-05 11:04:44 +0530

    Time: 924 ms (19.17%), Space: 53.6 MB (8.52%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
   int longestConsecutive(vector<int>& nums) {
-	unordered_set<int> s(begin(nums), end(nums));
-	int longest = 0;
-	for(auto& num : s) {
-        if(s.count(num - 1)) continue;
-		int j = 1;
-		while(s.count(num + j)) j++;
-		longest = max(longest, j);
-	}
-	return longest;
-}
+      unordered_map<int,pair<int,int>> m; //first low,second high
+      for(auto x:nums)
+              m[x]={x,x}; 
+      for(auto x:nums){
+          //add low and high
+          if(m.find(x-1)!=m.end())
+          {
+              m[x].first=min(m[x-1].first,m[x].first);
+              m[x].second=max(m[x-1].second,m[x].second);
+          }
+          if(m.find(x+1)!=m.end())
+          {
+              m[x].first=min(m[x].first,m[x+1].first);
+              m[x].second=max(m[x+1].second,m[x].second);
+          }
+      }
+      int mx=0,flag=1;
+      while(flag)
+      {
+          flag=0;
+          for(auto x:nums){
+          //add low and high
+              m[x].first=min(m[m[x].first].first,m[x].first);
+              m[x].second=max(m[m[x].second].second,m[x].second);
+      }
+          for(auto x:m)
+          {
+              if(x.second.second-x.second.first+1>mx)//means do it again
+              {
+                  mx=x.second.second-x.second.first+1;
+                  flag=1;
+              }
+              // mx=max(,mx);
+              // cout<<x.first<<" "<<x.second.first<<" "<<x.second.second<<endl;
+          }
+          
+      }
+      return mx;
+  }
 };
