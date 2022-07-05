4cd30e30342b9fcaffb4f96eb2cfd057b0840af2
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-05 15:06:03 +0530

    Time: 3 ms (81.27%), Space: 6.4 MB (92.61%) - LeetHub

--- 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
+++ 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
@@ -3,47 +3,28 @@ class Solution {
 public:
     bool solve(int i,string &num,int prev,int prevprev)
     {
-        // if((long long)prev+prevprev>=INT_MAX)return 0;
-        // cout<<i<<" "<<prev<<" "<<prevprev<<endl;
         if(i==num.size()&&ans.size()>=3)
-        {
             return true;
-        }
-        //if first means prev and prevprev not assigned then choose any values
-        long long val=0,sum=(long long)prev+prevprev;
-        //if leading zero then only one time it should run
-        if(num[i]=='0')
-        {
-              if(prev==-1||prevprev==-1||val==sum)
-              {
-                 ans.push_back(val);
-                if(solve(i+1,num,val,prev))
-                    return true;
-                 ans.pop_back(); 
-              }
-            return 0;
-        }
-            
-        for(int j=i;j<num.size();j++)
+        long long val=0;
+        bool lead=num[i]!='0';
+        
+        do
         {
             val*=10;
-            val+= num[j]-'0';
+            val+= num[i]-'0';
             if(val>=INT_MAX)return 0;
-            if(prev==-1||prevprev==-1||val==sum)
+            if(prev==-1||prevprev==-1||val==(long long)prev+prevprev)
             {
                 ans.push_back(val);
-                if(solve(j+1,num,val,prev))
+                if(solve(i+1,num,val,prev))
                     return true;
                  ans.pop_back();
             }
-        }
+            i++;
+        }while(lead&&i<num.size());//if leading zero then only one time it should run
         return 0;
     }
     vector<int> splitIntoFibonacci(string num) {
-        //1101111
-        //first choose number
-        //it should satisfy previous two sum 
-        //if it is saller we increase number
         solve(0,num,-1,-1);
         return ans;
     }
