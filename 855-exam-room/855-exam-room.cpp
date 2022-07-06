bd589030a07884196fe8106a74bdbeb0392e00ca
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-06 21:30:45 +0530

    Time: 800 ms (37.03%), Space: 19 MB (99.78%) - LeetHub

---
+++ 855-exam-room/855-exam-room.cpp
@@ -0,0 +1,43 @@
+class ExamRoom {
+    vector<int> students;
+    int N;
+public:
+    ExamRoom(int n) {
+        N = n;
+        students.clear();
+    }
+    
+    int seat() {
+        if(students.empty())
+        {
+            students.push_back(0);
+            return 0;
+        }
+        int d = max(students[0] - 0, N-1-students[students.size()-1]);
+        for(int i=0; i<students.size()-1; i++)
+            d = max(d, (students[i+1]- students[i])/2);
+        if(students[0]==d)
+        {
+            students.insert(students.begin(), 0);
+            return 0;
+        }
+        for(int i=0; i<students.size()-1; i++)
+        {
+            if((students[i+1]-students[i])/2==d)
+            {
+                students.insert(students.begin()+i+1, (students[i+1]+students[i])/2);
+                return students[i+1];
+            }
+        }
+        students.push_back(N-1);
+        return N-1;
+    }
+    
+    void leave(int p) {
+        for(int i=0; i<students.size(); i++)
+        {
+            if(students[i]==p)
+                students.erase(students.begin()+i);
+        }
+    }
+};
