bbd71364744534a7c0f62c5fa53bad511b2b94ef
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-07 10:08:04 +0530

    Time: 9 ms (43.08%), Space: 14.4 MB (47.64%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -10,54 +10,16 @@
  * };
  */
 class Solution {
-    int mx=0,count=0;
-    TreeNode *ans=0;
 public:
-    void solve(TreeNode* root,int level)
-    {
-        if(root)
-        {
-            solve(root->left,level+1);
-            solve(root->right,level+1);
-            if(level>mx)
-            {
-                mx=level;
-                count=0;
-            }
-            if(level==mx)//means this is the last nodes return its root 
-                count++;
-            // cout<<root->val<<" "<<count<<" "<<mx<<endl;
-        }
+     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return deep(root).second;
     }
-    int solve2(TreeNode* root,int level)
-    {
-        if(root)
-        {
-            int x=solve2(root->left,level+1);
-            int y=solve2(root->right,level+1);
-            x+=(level==mx);//this node must be included
-            if(x+y==count)//means this is the ans
-            {
-                ans=root;
-                count=-1;
-                return 0;
-            }
-            else if(x||y)//if any one node includes the count
-            {
-                return x+y;
-            }
-            // cout<<root->val<<" "<<count<<" "<<mx<<endl;
-        }
-        return 0;
-    }
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        //first of all find deepest nodes
-        //return that subtree //check on other side if we got any deepest subtree
-        //this is levelorder traversal we need to find max level 
-        //first find max level then simply push all the nodes that are coming in that
-        solve(root,0);//this will help us to count deep nodes
-        // cout<<count<<" "<<mx;
-        solve2(root,0);
-        return ans;
+
+    pair<int, TreeNode*> deep(TreeNode* root) {
+        if (!root) return {0, NULL};
+        pair<int, TreeNode*> l = deep(root->left), r = deep(root->right);
+
+        int d1 = l.first, d2 = r.first;
+        return {max(d1, d2) + 1, d1 == d2 ? root : d1 > d2 ? l.second : r.second};
     }
 };
