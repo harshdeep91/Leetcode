2ad6cc5a1aa9ac38cc43f2a26b2aefd1c448ac3f
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-11 09:12:09 +0530

    Time: 10 ms (75.91%), Space: 8.6 MB (91.97%) - LeetHub

--- 893-groups-of-special-equivalent-strings/893-groups-of-special-equivalent-strings.cpp
+++ 893-groups-of-special-equivalent-strings/893-groups-of-special-equivalent-strings.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    void convert(string &a)//converting to sorted even and odd
-    {
-        string even,odd;
-        bool flag=1
-            ;
-        for(int i=0;i<a.size();i++)
-        {
-            if(flag)//even
-                even.push_back(a[i]);
-            else
-                odd.push_back(a[i]);
-            flag=!flag;
-        }
-        sort(even.begin(),even.end());
-        sort(odd.begin(),odd.end());
-        a=even+odd;
-    }
     int numSpecialEquivGroups(vector<string>& words) {
-        //Brute Force= check by 2 for loops N*k*N*k
-        // sort by even and odd index ,sort main string,sliding window,nklogk
-        // we can also store count in even and odd index and check
-        for(int i=0;i<words.size();i++)
-            convert(words[i]);
-         sort(words.begin(),words.end());
-        unordered_map<string,int> m;
-        for(auto x:words)
-            m[x]++;
-        return m.size();
+        unordered_set<string> eqs;
+        for (const auto& word : words) {
+            string sEven;
+            string sOdd;
+            for (int i = 0; i < word.size(); ++i) {
+                if (i % 2) {
+                    sOdd.push_back(word[i]);
+                } else {
+                    sEven.push_back(word[i]);
+                }
+            }
+            
+            sort(sEven.begin(), sEven.end());
+            sort(sOdd.begin(), sOdd.end());
+            eqs.insert(sEven + sOdd);
+        }
+        return eqs.size();
     }
 };
