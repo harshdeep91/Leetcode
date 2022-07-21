bcae52018a0fc2765c5d9d4a1e4c866ac5377ee8
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-21 14:41:41 +0530

    Time: 3 ms (93.57%), Space: 8.5 MB (29.17%) - LeetHub

--- 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cpp
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+//     Algorithm: Start from the last step, and go back to the first step.
+// Our vector is [2,13,3,11,5,17,7].
+// The last step is [17], which is the largest element. [17]
+// Before 17, the number should be 13. Of course, the vector can be [13, 17], as we pop out 13, then there is 1 element in the vector, it does not matter whether we rotate the vector.[13 ,17]
+// The next element should be 11. If we wanna make the vector like [13, 17] after popping out 11, then 17 should be the next element after 11. Because once we pop out 13, the 17 can be moved to the end of vector. so now the vector is [11, 17, 13]
+// The next element is 7. Similar as previous step, if we want to make the vector to be [11, 17, 13] after popping out 7, then 13 is the next element after 7, so the vector is [7, 13, 11, 17]
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         // Sort by increasing order.
         sort(deck.begin(), deck.end());
