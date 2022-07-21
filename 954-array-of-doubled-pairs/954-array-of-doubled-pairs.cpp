64ab9c2cc36cb9503db629d0de21556e9d119cf6
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-21 16:00:16 +0530

    Time: 314 ms (14.82%), Space: 58 MB (82.12%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
+    static bool cmp(int a,int b)
+    {
+        if(a<0&&b<0)
+            return a>b;
+        else
+            return a<b;
+            
+    }
     bool canReorderDoubled(vector<int>& arr) {
         //arr lenth is even means there is always a even value in odd index
-            vector<int> pos,neg;
             unordered_map<int,int> m;
-            for(int i=0;i<arr.size();i++)
-            {
-                if(arr[i]<0)
-                    neg.push_back(arr[i]);
-                else
-                pos.push_back(arr[i]);
-                m[arr[i]]++;
-            }
-            sort(neg.begin(),neg.end()),sort(pos.begin(),pos.end());
-            for(int i=neg.size()-1;i>=0;i--)
-                pos.push_back(neg[i]);
+            sort(arr.begin(),arr.end(),cmp);
+            for(int i=0;i<arr.size();i++)m[arr[i]]++;
         //store the max element first 
         //simply find if it is possible to get val,2*val pairs
         
 
-        for(int i=0;i<pos.size();i++)
+        for(int i=0;i<arr.size();i++)
         {
-            if(m[pos[i]])
+            if(m[arr[i]])
             {
-                if(m[2*pos[i]])
+                if(m[2*arr[i]])
                 {
-                    m[pos[i]]--;
-                    m[2*pos[i]]--;
+                    m[arr[i]]--;
+                    m[2*arr[i]]--;
                 }
                 else return 0;
             }
