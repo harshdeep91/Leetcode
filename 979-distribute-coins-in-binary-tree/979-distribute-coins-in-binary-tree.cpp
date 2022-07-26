4227a52f946c950fda62d0db7ca5a3dfb58eb60d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-26 17:52:17 +0530

    Time: 18 ms (9.91%), Space: 14.1 MB (8.40%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int count=0;
+    // TreeNode *head=0;
+public:
+    int solve(TreeNode *root)
+    {
+        if(root)
+        {
+            int x=solve(root->left);
+            int y=solve(root->right);
+            //send the extra and required coins up
+            //and process the coins comming from down;
+            //if coins is extra then it is positve else if required negative
+            
+            int extra= root->val-1+x+y;
+            count+= abs(extra);
+            return extra;
+            cout<<root->val<<" "<<extra<<" "<<x<<" "<<y<<" "<<count<<endl;
+        }
+        return 0;
+    }
+    int distributeCoins(TreeNode* root) {
+        //travel postorder first
+        //after that send the requirment to parents
+        //if parent have more then 1 coin subtract extra coins
+        //moving coins from top to bottom will take 1 unit per level
+        
+         // cout<<solve(root)<<" "<<count<<endl; 
+        // return abs(solve(root)-count);
+        solve(root);
+        return count;
+    }
+};
