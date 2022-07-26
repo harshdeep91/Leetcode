f38c1a7b580e25834804974450fac8c5a0b06b4c
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-26 17:02:18 +0530

    Time: 128 ms (42.19%), Space: 40.4 MB (61.22%) - LeetHub

--- 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int greater,tmp,less,ans=1,countg=1,countl=1;
-        // greater=less=arr[0];
+        int tmp,ans=1,countg=1,countl=1;
         for(int i=1;i<arr.size();i++)
         {
             //for greater we need less<arr[i]
             //if exist do it otherewise reset
              tmp=arr[i-1]<arr[i]?countl+1:1;
-             // greater=arr[i];
              countl=arr[i-1]>arr[i]?countg+1:1;
              countg=tmp;
-            // cout<<arr[i]<<" "<<count
              ans=max(ans,max(countg,countl));
             //
         }
