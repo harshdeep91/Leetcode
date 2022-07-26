dc3ac54d11cffd100cb1f764851aa3e80412c338
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-26 22:19:37 +0530

    Time: 8 ms (44.75%), Space: 9.9 MB (31.11%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,28 +1,12 @@
 class Solution {
-    int mi=INT_MAX;
-    int day[3]={0,6,29};
-    vector<int> dp;
 public:
-    int solve(int i,vector<int>& days, vector<int>& costs)
-    {
-        
-        if(i==days.size())
-            return 0;
-        if(dp[i]!=-1)return dp[i];
-        // cout<<days[i]<<" "<<cost<<endl;
-        //choose day 1
-        int mi=INT_MAX,it;
-        for(int j=0;j<3;j++)
-        {
-            it = upper_bound(days.begin(),days.end(),days[i]+day[j])-days.begin();
-            mi=min(mi,solve(it,days,costs)+costs[j]);
-        }
-        return dp[i]=mi;
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        //choose day 1 ,day 7 , day 30
-        //either we go day with cost 
-        dp.resize(days.size(),-1);
-        return solve(0,days,costs);
-    }
+  unordered_set<int> travel(begin(days), end(days));
+  int dp[366] = {};
+  for (int i = 1; i < 366; ++i) {
+    if (travel.find(i) == travel.end()) dp[i] = dp[i - 1];
+    else dp[i] = min({ dp[i - 1] + costs[0], dp[max(0, i - 7)] + costs[1], dp[max(0, i - 30)] + costs[2]});
+  }
+  return dp[365];
+}
 };
