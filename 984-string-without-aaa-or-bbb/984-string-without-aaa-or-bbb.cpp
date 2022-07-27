33dd89222a422402440fc4ccfbeb1098f7acb0b5
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-27 20:15:29 +0530

    Time: 7 ms (8.72%), Space: 6.2 MB (30.22%) - LeetHub

--- 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cpp
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cpp
@@ -1,46 +1,12 @@
 class Solution {
 public:
-    string solve(int a,int b,char aa,char bb)
-    {
-        int val_a= a/2,val_b=b/2,split=b%2,extra_a=a%2;
-        //we need vala-1 b's
-        //if(val_b<val_a-1)split them into 2
-        //if(val_b>val_a-1)split avoid split
-         string ans;
-        if(a==1)
-        {
-            ans.push_back(aa);
-            while(b--)ans.push_back(bb);
-            return ans;
-        }
-         while(val_b+split<val_a-1+extra_a)
-         {
-             split+=2;
-             val_b--;
-         }
-        // cout<<val_b<<" "<<split<<" "<<val_a-1<<endl;
-        //first put a then b and 
-        while(ans.size()<a+b)
-        {
-            if(val_a)
-                ans.push_back(aa),ans.push_back(aa),val_a--;
-            else if(extra_a)
-                ans.push_back(aa),extra_a--;
-            if(val_b)
-                ans.push_back(bb),ans.push_back(bb),val_b--;
-            else if(split)
-                ans.push_back(bb),split--;
-        }
-        
-        return ans;
-    }
-    string strWithout3a3b(int a, int b) {
-        //see the recursion first
-        //we can simply put 2 a first
-        //then check how many min b we need
-        // it is simply value-1
-        //if b is greater put them in 2 digits
-        return  (a<b)?solve(b,a,'b','a'):solve(a,b,'a','b');
-        
-    }
+    string strWithout3a3b(int A, int B, char a = 'a', char b = 'b', string res = "") {
+  if (B > A) return strWithout3a3b(B, A, b, a);
+  while (A-- > 0) {
+    res += a;
+    if (A > B) res += a, A--;
+    if (B-- > 0) res += b;
+  }
+  return res;
+}
 };
