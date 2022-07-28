bbe085e6685590d5cac556b19adb100f8df833f0
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-07-28 19:34:10 +0530

    Time: 56 ms (37.14%), Space: 26.4 MB (98.68%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        // 2 4 
-        //check decreasing order if exist then find smallest index that is greater
-        stack<int> s;
-        int mi=nums.size(),mx=-1,prev,curr;
-        for(int i=0;i<nums.size();i++)
+        //find first min from right
+        //if any number not satify it then min index is end and number index is s
+        
+        int n=nums.size(), mi=nums[n-1],e=-1,s=-1,index=n-1,mx=nums[0];
+        for(int i=n-2;i>=0;i--)
         {
-            curr=i;
-            if(!s.empty()&&nums[s.top()]>nums[i])
-                curr=s.top(),s.pop();
-            prev=curr;
-            while(!s.empty()&&nums[s.top()]>nums[i])
-                prev=s.top(),s.pop();
-            if(curr!=i&&nums[curr]>=nums[i])
+            if(nums[i]<=mi)//update mi
+                mi=nums[i],index=i;
+            else //means we got the new size
             {
-                mi=min(mi,prev);
-                mx=max(mx,i);
-                s.push(prev);
-                if(prev!=curr)
-                s.push(curr);
+                if(e==-1)e=index;
+                s=i;
             }
-            else
-            s.push(i);
-            // cout<<mi<<" "<<mx<<endl;
         }
-            return mx==-1?0:mx-mi+1;
+        // cout<<s<<" "<<e<<endl;
+        //find first max from left
+        index=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>=mx)//update mx
+                mx=nums[i],index=i;
+            else //means we got the new size
+            {
+                if(s==-1)s=index,e=i;
+                else
+                    s=min(s,index),e=max(e,i);
+            }
+        }
+        // cout<<s<<" "<<e<<endl;
+        return e==-1?0:e-s+1;
     }
 };
