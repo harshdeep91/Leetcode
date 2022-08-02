47e2dff5b13ce7db437a278c560c38a170b2e91d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-08-02 17:59:28 +0530

    Time: 142 ms (5.24%), Space: 47 MB (5.02%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -5,28 +5,9 @@ public:
         //maximum value is possible when left is max and right is also max
         vector<int> f,s;
         for(int i=0;i<values.size();i++)f.push_back(values[i]+i),s.push_back(values[i]-i);
-        int mx=f[0];
-        for(int i=1;i<values.size();i++)
-        {
-            if(f[i]>mx)
-                mx=f[i];
-             else
-                 f[i]=mx;
-               
-        }
-        for(int i=0;i<values.size();i++)cout<<f[i]<<" ";
-        cout<<endl;
-        mx=s.back();
-        for(int i=s.size()-2;i>=0;i--)
-        {
-            if(s[i]>mx)
-                mx=s[i];
-             else
-                 s[i]=mx;
-        }
-        for(int i=0;i<values.size();i++)cout<<s[i]<<" ";
-        cout<<endl;
-        mx=0;
+        for(int i=1;i<values.size();i++)f[i]=max(f[i],f[i-1]);
+        for(int i=s.size()-2;i>=0;i--)s[i]=max(s[i],s[i+1]);
+        int mx=0;
         for(int i=0;i<values.size()-1;i++)
             mx=max(mx,f[i]+s[i+1]);
         return mx;
