cc7299d6b02c2539d6031c9f61293bca08a8dc7d
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-08-03 10:29:49 +0530

    Time: 1508 ms (5.76%), Space: 5.9 MB (77.34%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        
-          int n=0,carry=0,end=1e7;
+        if(k==1)return 1;
+          int n=0,carry=0,end=1e7,prev=0;
         do
         {
-            carry= (carry*10+1)%k;
+            prev= (carry*10+1)%k;
             n++;
+            // cout<<prev<<" "<<carry<<endl;
+            if(prev==carry)return -1;
+            carry=prev;
         }while(n<end&&carry);
         return n<end?n:-1;
     }
