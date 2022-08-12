e11b153653ef9b8a9d728fed061dcb1382bcd0ce
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-08-12 22:52:51 +0530

    Time: 62 ms (16.68%), Space: 23.2 MB (63.86%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,47 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
-    TreeNode *ans=0;
 public:
-    int lca(TreeNode* root, TreeNode* p, TreeNode* q) 
-    {
-        if(root)
-        {
-            int x= lca(root->left,p,q);
-            int y= lca(root->right,p,q);
-            if(ans)return 0;
-            if(root==p||root==q)//return based on prev
-            {
-                if(x==1||y==1)//means we already got other element
-                    ans=root;
-                
-                    return 1;
-            }
-            if(x==1||y==1)
-            {
-                if(x==1&&y==1)
-                {
-                    if(!ans)ans=root;
-                }
-                else
-                    return 1;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
             }
         }
-        return 0;
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        //for finding lowest common ancestor we need to 
-        //travel by postorder if we got both element return the root
-        //if only one return the node we found
-        lca(root,p,q);
-        return ans;
+        return cur;
     }
 };
