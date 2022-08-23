c014abff11d31c9b4e015f62175d350cb50edb2a
harshdeep91 <87524408+harshdeep91@users.noreply.github.com>
2022-08-23 11:28:13 +0530

    Time: 3 ms (58.67%), Space: 5.8 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
+    //power of four is directly related to bits manipulation
+      // 16 and 15 will give 0 but in 3 2 11&10=>1 ,
+        //to separate power of 2 from power of 4 in power of 4 leftmost bit is at position odd
+        // n= n&(n-1);
+        // cout<<n;
         	return num > 0 && (num & (num - 1)) == 0 && (num & 0xAAAAAAAA) == 0;
 
+        // return !n;
     }
 };
